179e60ea3836c547a1a9307074764ba427b7f1bc
User <ulyanovskiy.2001@mail.ru>
2026-04-16 11:49:18 +0300

    feat: update parse num benchmark

--- benchmarks/parse_num_f64.ts
+++ benchmarks/parse_num_f64.ts
@@ -14,17 +14,17 @@ const integerSlowpath = encoder.encode("1123456789123456789123456789")
 const floatSlowPathNum = encoder.encode("1.123456789123456789123456789")
 
 suite
-    .add('integerFastpath', () => parseNumberF64(integerFastpath, 0, integerFastpath.length))
+    .add('integerFastpath', () => parseNumberF64(integerFastpath, 0, integerFastpath.length, integerFastpath.length))
     .add('integerFastpathDecoder', () => Number(decoder.decode(integerFastpath)))
-    .add('floatFastpath', () => parseNumberF64(floatFastpath, 0, floatFastpath.length))
+    .add('floatFastpath', () => parseNumberF64(floatFastpath, 0, floatFastpath.length, floatFastpath.length))
     .add('floatFastpathDecoder', () => Number(decoder.decode(floatFastpath)))
-    .add('integerMidpath', () => parseNumberF64(integerMidpath, 0, integerMidpath.length))
+    .add('integerMidpath', () => parseNumberF64(integerMidpath, 0, integerMidpath.length, integerMidpath.length))
     .add('integerMidpathDecoder', () => Number(decoder.decode(integerMidpath)))
-    .add('floatMidpath', () => parseNumberF64(floatMidpath, 0, floatMidpath.length))
+    .add('floatMidpath', () => parseNumberF64(floatMidpath, 0, floatMidpath.length, floatMidpath.length))
     .add('floatMidpathDecoder', () => Number(decoder.decode(floatMidpath)))
-    .add('integerSlowpath', () => parseNumberF64(integerSlowpath, 0, integerSlowpath.length))
+    .add('integerSlowpath', () => parseNumberF64(integerSlowpath, 0, integerSlowpath.length, integerSlowpath.length))
     .add('integerSlowpathDecoder', () => Number(decoder.decode(integerSlowpath)))
-    .add('floatSlowpath', () => parseNumberF64(floatSlowPathNum, 0, floatSlowPathNum.length))
+    .add('floatSlowpath', () => parseNumberF64(floatSlowPathNum, 0, floatSlowPathNum.length, floatSlowPathNum.length))
     .add('floatSlowpathDecoder', () => Number(decoder.decode(floatSlowPathNum)))
 
 suite.run().then(() => {
