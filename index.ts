98e083d6ec0af126c546a716cb2569b86c7449d3
User <ulyanovskiy.2001@mail.ru>
2026-04-28 19:12:07 +0300

    feat: update main file

--- index.ts
+++ index.ts
@@ -2,30 +2,27 @@ import { deserialize } from "./src/json"
 import { toMetadata } from "./src/metadata/metadata"
 
 const obj = {
-    id: 15,
-    order: 1244,
+    id: 343543534,
+    order: 343543534,
     phone: 343543534,
     phone1: 343543534,
-    phone2: 343543534,
-    phone3: 343543534,
-    phone4: 343543534,
-    phone5: 343543534,
-    phone6: 343543534,
-    phone7: 343543534,
-    phone8: 343543534,
-    phone9: 343543534,
-    phone10: 343543534,
-    phone11: 343543534,
-    phone12: 343543534,
-    phone13: 343543534,
-    // phone: {
-    //     code: 7
-    // },
+    tags_count: 343543534,
+    owner_id: 343543534,
+    budget: 343543534,
+    risk_level: 343543534,
+    retry_count: 343543534,
+    max_retries: 343543534,
+    timeout_seconds: 343543534,
+    storage_gb: 343543534,
+    cpu_cores: 343543534,
+    memory_mb: 343543534,
+    rate_limit_rps: 343543534,
+    data_retention_days: 343543534
 }
 
 const metadata = toMetadata(obj)
 
-const value = JSON.stringify(obj, null, 4)
+const value = JSON.stringify(obj, null, 8)
 
 const deserialized = deserialize(new TextEncoder().encode(value), metadata)
 
