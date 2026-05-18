6daf18c461d09966890b9c5c633ffb9eed311ee0
User <ulyanovskiy.2001@mail.ru>
2026-05-18 14:58:19 +0300

    tests: update parsenumf64 benchnmark

--- benchmarks/parse_num_f64.ts
+++ benchmarks/parse_num_f64.ts
@@ -1,7 +1,5 @@
-import { Bench } from 'tinybench'
 import { parseNumberF64 } from '../src/converters/number'
-
-const suite = new Bench({ name: 'object_creation', warmupIterations: 200 })
+import { add, complete, cycle, suite } from 'benny'
 
 const encoder = new TextEncoder()
 const decoder = new TextDecoder()
@@ -13,21 +11,30 @@ const floatMidpath = encoder.encode("90071992.54740992")
 const integerSlowpath = encoder.encode("1123456789123456789123456789")
 const floatSlowPathNum = encoder.encode("1.123456789123456789123456789")
 
-suite
-    .add('integerFastpath', () => parseNumberF64(integerFastpath, 0))
-    .add('integerFastpathDecoder', () => Number(decoder.decode(integerFastpath)))
-    .add('floatFastpath', () => parseNumberF64(floatFastpath, 0))
-    .add('floatFastpathDecoder', () => Number(decoder.decode(floatFastpath)))
-    .add('integerMidpath', () => parseNumberF64(integerMidpath, 0))
-    .add('integerMidpathDecoder', () => Number(decoder.decode(integerMidpath)))
-    .add('floatMidpath', () => parseNumberF64(floatMidpath, 0))
-    .add('floatMidpathDecoder', () => Number(decoder.decode(floatMidpath)))
-    .add('integerSlowpath', () => parseNumberF64(integerSlowpath, 0))
-    .add('integerSlowpathDecoder', () => Number(decoder.decode(integerSlowpath)))
-    .add('floatSlowpath', () => parseNumberF64(floatSlowPathNum, 0))
-    .add('floatSlowpathDecoder', () => Number(decoder.decode(floatSlowPathNum)))
+suite(
+    'decoding',
 
-suite.run().then(() => {
-    console.log(suite.name)
-    console.table(suite.table())
-})
+    add('integerFastpath', () => parseNumberF64(integerFastpath, 0)),
+    add('integerFastpathDecoder', () => Number(decoder.decode(integerFastpath))),
+    add('floatFastpath', () => parseNumberF64(floatFastpath, 0)),
+    add('floatFastpathDecoder', () => Number(decoder.decode(floatFastpath))),
+    add('integerMidpath', () => parseNumberF64(integerMidpath, 0)),
+    add('integerMidpathDecoder', () => Number(decoder.decode(integerMidpath))),
+    add('floatMidpath', () => parseNumberF64(floatMidpath, 0)),
+    add('floatMidpathDecoder', () => Number(decoder.decode(floatMidpath))),
+    add('integerSlowpath', () => parseNumberF64(integerSlowpath, 0)),
+    add('integerSlowpathDecoder', () => Number(decoder.decode(integerSlowpath))),
+    add('floatSlowpath', () => parseNumberF64(floatSlowPathNum, 0)),
+    add('floatSlowpathDecoder', () => Number(decoder.decode(floatSlowPathNum))),
+
+    cycle((result) => {
+        const nanoseconds = (1 / result.ops) * 1e9
+        console.log(
+            `${result.name}: ` +
+            `${result.ops.toLocaleString()} ops/s, ` +
+            `${nanoseconds.toFixed(2)} ns/op`
+        )
+    }),
+
+    complete(),
+)
