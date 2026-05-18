82f0dea722b98228be96bfe35fa3ce7b45985d57
User <ulyanovskiy.2001@mail.ru>
2026-05-18 14:55:55 +0300

    tests: update parse_num_64 benchmark

--- benchmarks/parse_num_f64.ts
+++ benchmarks/parse_num_f64.ts
@@ -14,17 +14,17 @@ const integerSlowpath = encoder.encode("1123456789123456789123456789")
 const floatSlowPathNum = encoder.encode("1.123456789123456789123456789")
 
 suite
-    .add('integerFastpath', () => parseNumberF64(integerFastpath, 0, integerFastpath.length, integerFastpath.length))
+    .add('integerFastpath', () => parseNumberF64(integerFastpath, 0))
     .add('integerFastpathDecoder', () => Number(decoder.decode(integerFastpath)))
-    .add('floatFastpath', () => parseNumberF64(floatFastpath, 0, floatFastpath.length, floatFastpath.length - 1))
+    .add('floatFastpath', () => parseNumberF64(floatFastpath, 0))
     .add('floatFastpathDecoder', () => Number(decoder.decode(floatFastpath)))
-    .add('integerMidpath', () => parseNumberF64(integerMidpath, 0, integerMidpath.length, integerMidpath.length))
+    .add('integerMidpath', () => parseNumberF64(integerMidpath, 0))
     .add('integerMidpathDecoder', () => Number(decoder.decode(integerMidpath)))
-    .add('floatMidpath', () => parseNumberF64(floatMidpath, 0, floatMidpath.length, floatMidpath.length))
+    .add('floatMidpath', () => parseNumberF64(floatMidpath, 0))
     .add('floatMidpathDecoder', () => Number(decoder.decode(floatMidpath)))
-    .add('integerSlowpath', () => parseNumberF64(integerSlowpath, 0, integerSlowpath.length, integerSlowpath.length))
+    .add('integerSlowpath', () => parseNumberF64(integerSlowpath, 0))
     .add('integerSlowpathDecoder', () => Number(decoder.decode(integerSlowpath)))
-    .add('floatSlowpath', () => parseNumberF64(floatSlowPathNum, 0, floatSlowPathNum.length, floatSlowPathNum.length))
+    .add('floatSlowpath', () => parseNumberF64(floatSlowPathNum, 0))
     .add('floatSlowpathDecoder', () => Number(decoder.decode(floatSlowPathNum)))
 
 suite.run().then(() => {
