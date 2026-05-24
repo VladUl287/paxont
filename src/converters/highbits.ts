ada35ce57548c9e8a479d2b9c8f44981bac70460
User <ulyanovskiy.2001@mail.ru>
2026-05-24 17:44:33 +0300

    feat: high bits 5 digits chunk

--- src/converters/highbits.ts
+++ src/converters/highbits.ts
@@ -1,16 +1,18 @@
 const registers = new Uint32Array(3)
 
 export function getHigh64BitsFrom96(digits: Uint8Array | number[], base = 10) {
-    const CHUNK_MUL = Math.pow(base, 4)
+    const CHUNK_SIZE = 5
+    const CHUNK_MUL = Math.pow(base, CHUNK_SIZE)
 
     let i = 0
-    for (; i < digits.length - 4; i += 4) {
+    for (; i < digits.length - CHUNK_SIZE; i += CHUNK_SIZE) {
         const a = digits[i]
         const b = digits[i + 1]
         const c = digits[i + 2]
         const d = digits[i + 3]
+        const e = digits[i + 4]
 
-        let carry = (((a * 10) + b) * 10 + c) * 10 + d
+        let carry = ((((a * 10) + b) * 10 + c) * 10 + d) * 10 + e
 
         let product = registers[0] * CHUNK_MUL + carry
         registers[0] = product & 0xFFFFFFFF
