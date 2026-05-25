3fe4ade15c192c67aa6f5f396d61d6baa8732936
User <ulyanovskiy.2001@mail.ru>
2026-05-25 14:04:11 +0300

    feat: high bits use correct high construction

--- src/converters/highbits.ts
+++ src/converters/highbits.ts
@@ -1,12 +1,14 @@
-const registers = new Uint32Array(3)
+const registers = new Uint32Array(128)
 
 export function getHigh64BitsFrom96(digits: Uint8Array | number[], base = 10) {
     const CHUNK_SIZE = 5
     const CHUNK_MUL = Math.pow(base, CHUNK_SIZE)
-
     const UINT32_MAX = 0xFFFFFFFF
+    const TWO_POW_32 = 0x100000000
 
     let i = 0
+    let count = Math.max(2, Math.ceil(digits.length * Math.log2(10) / 32) + 1)
+
     for (; i < digits.length - CHUNK_SIZE; i += CHUNK_SIZE) {
         const a = digits[i]
         const b = digits[i + 1]
@@ -16,31 +18,31 @@ export function getHigh64BitsFrom96(digits: Uint8Array | number[], base = 10) {
 
         let carry = ((((a * 10) + b) * 10 + c) * 10 + d) * 10 + e
 
-        let product = registers[0] * CHUNK_MUL + carry
-        registers[0] = product & UINT32_MAX
-        carry = (product / 0x100000000) | 0
+        for (let i = 0; i < count; i++) {
+            const product = registers[i] * CHUNK_MUL + carry
+            registers[i] = product & UINT32_MAX
+            carry = (product / TWO_POW_32) | 0
+        }
 
-        product = registers[1] * CHUNK_MUL + carry
-        registers[1] = product & UINT32_MAX
-        carry = (product / 0x100000000) | 0
-
-        product = registers[2] * CHUNK_MUL + carry
-        registers[2] = product & UINT32_MAX
+        if (carry > 0) {
+            registers[count] = carry
+            count++
+        }
     }
 
     for (; i < digits.length; i++) {
         let carry = digits[i]
 
-        let product = registers[0] * base + carry
-        registers[0] = product & UINT32_MAX
-        carry = (product / 0x100000000) | 0
+        for (let i = 0; i < count; i++) {
+            const product = registers[i] * 10 + carry
+            registers[i] = product & UINT32_MAX
+            carry = (product / TWO_POW_32) | 0
+        }
 
-        product = registers[1] * base + carry
-        registers[1] = product & UINT32_MAX
-        carry = (product / 0x100000000) | 0
-
-        product = registers[2] * base + carry
-        registers[2] = product & UINT32_MAX
+        if (carry > 0) {
+            registers[count] = carry
+            count++
+        }
     }
 
     return registers
