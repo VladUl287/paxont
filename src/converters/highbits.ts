363371257eabbe9c29ef349f508d9ecf389b0360
User <ulyanovskiy.2001@mail.ru>
2026-05-26 11:22:41 +0300

    feat: increase chunk up to 6

--- src/converters/highbits.ts
+++ src/converters/highbits.ts
@@ -1,55 +1,58 @@
-const registers96 = new Uint32Array(3)
+const buffer96 = new ArrayBuffer(16)
+const registers96_U32 = new Uint32Array(buffer96)
+const registers96_U64 = new BigUint64Array(buffer96)
 
-export function getHigh64BitsFrom96(digits: Uint8Array | number[], base = 10) {
-    const CHUNK_SIZE = 5
+export function getHigh64BitsFrom96(digits: Uint8Array | number[], length: number, base = 10) {
+    const CHUNK_SIZE = 6
     const CHUNK_MUL = Math.pow(base, CHUNK_SIZE)
 
     const UINT32_MAX = 0xFFFFFFFF
 
     let i = 0
-    for (; i < digits.length - CHUNK_SIZE; i += CHUNK_SIZE) {
-        const a = digits[i]
-        const b = digits[i + 1]
-        const c = digits[i + 2]
-        const d = digits[i + 3]
-        const e = digits[i + 4]
+    for (; i < length - CHUNK_SIZE; i += CHUNK_SIZE) {
+        let carry =
+            digits[i] * 100000 +
+            digits[i + 1] * 10000 +
+            digits[i + 2] * 1000 +
+            digits[i + 3] * 100 +
+            digits[i + 4] * 10 +
+            digits[i + 5]
 
-        let carry = ((((a * 10) + b) * 10 + c) * 10 + d) * 10 + e
-
-        let product = registers96[0] * CHUNK_MUL + carry
-        registers96[0] = product & UINT32_MAX
+        let product = registers96_U32[0] * CHUNK_MUL + carry
+        registers96_U32[0] = product & UINT32_MAX
         carry = (product / 0x100000000) | 0
 
-        product = registers96[1] * CHUNK_MUL + carry
-        registers96[1] = product & UINT32_MAX
+        product = registers96_U32[1] * CHUNK_MUL + carry
+        registers96_U32[1] = product & UINT32_MAX
         carry = (product / 0x100000000) | 0
 
-        product = registers96[2] * CHUNK_MUL + carry
-        registers96[2] = product & UINT32_MAX
+        product = registers96_U32[2] * CHUNK_MUL + carry
+        registers96_U32[2] = product & UINT32_MAX
     }
 
-    for (; i < digits.length; i++) {
+    for (; i < length; i++) {
         let carry = digits[i]
 
-        let product = registers96[0] * base + carry
-        registers96[0] = product & UINT32_MAX
+        let product = registers96_U32[0] * base + carry
+        registers96_U32[0] = product & UINT32_MAX
         carry = (product / 0x100000000) | 0
 
-        product = registers96[1] * base + carry
-        registers96[1] = product & UINT32_MAX
+        product = registers96_U32[1] * base + carry
+        registers96_U32[1] = product & UINT32_MAX
         carry = (product / 0x100000000) | 0
 
-        product = registers96[2] * base + carry
-        registers96[2] = product & UINT32_MAX
+        product = registers96_U32[2] * base + carry
+        registers96_U32[2] = product & UINT32_MAX
     }
 
-    return registers96
+    return registers96_U32
 }
 
 const registers = new Uint32Array(128)
 export function getHigh64Bits(digits: Uint8Array | number[], base = 10) {
-    const CHUNK_SIZE = 5
+    const CHUNK_SIZE = 6
     const CHUNK_MUL = Math.pow(base, CHUNK_SIZE)
+
     const UINT32_MAX = 0xFFFFFFFF
     const TWO_POW_32 = 0x100000000
 
@@ -57,13 +60,13 @@ export function getHigh64Bits(digits: Uint8Array | number[], base = 10) {
     let count = Math.max(2, Math.ceil(digits.length * Math.log2(10) / 32) + 1)
 
     for (; i < digits.length - CHUNK_SIZE; i += CHUNK_SIZE) {
-        const a = digits[i]
-        const b = digits[i + 1]
-        const c = digits[i + 2]
-        const d = digits[i + 3]
-        const e = digits[i + 4]
-
-        let carry = ((((a * 10) + b) * 10 + c) * 10 + d) * 10 + e
+        let carry =
+            digits[i] * 100000 +
+            digits[i + 1] * 10000 +
+            digits[i + 2] * 1000 +
+            digits[i + 3] * 100 +
+            digits[i + 4] * 10 +
+            digits[i + 5]
 
         for (let i = 0; i < count; i++) {
             const product = registers[i] * CHUNK_MUL + carry
