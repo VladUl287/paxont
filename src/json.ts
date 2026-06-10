89330847fa23f17e4abe8c4fe9752da4c95e905a
User <ulyanovskiy.2001@mail.ru>
2026-06-10 12:35:40 +0300

    feat: create couple deserializer functions for async and new signature

--- src/json.ts
+++ src/json.ts
@@ -2,18 +2,46 @@ import { isError } from "./converters/types"
 import { Metadata } from "./metadata/metadata"
 import { defaultOptions, JsonOptions, mergerOptions } from "./options/types"
 import { createCache } from "./cache/cache"
+import { BaseMeta, ObjectMeta } from "./metadata/types"
 
 const optionsCache = createCache<Partial<JsonOptions>, JsonOptions>()
 
-export function deserialize<T>(json: Uint8Array<ArrayBuffer>, metadata: Metadata, options?: Partial<JsonOptions>): T {
+// export function deserialize1<T, M extends BaseMeta<T> = BaseMeta<T>>(
+//     json: Uint8Array,
+//     meta: BaseMeta<T> | T,
+//     options?: Partial<JsonOptions>
+// ): T {
+//     return {} as any
+// }
+
+export function deserialize1<T, M extends BaseMeta<T> = BaseMeta<T>>(
+    json: Uint8Array,
+    meta: M | T,
+    options?: Partial<JsonOptions>
+): T {
+    return {} as any
+}
+
+deserialize1(new Uint8Array(), {} as ObjectMeta<{ id: number }>)
+deserialize1(new Uint8Array(), { id: 1 })
+
+export async function deserializeAsync<T, M extends BaseMeta<T> = BaseMeta<T>>(
+    stream: ReadableStream,
+    meta: M | T,
+    options?: Partial<JsonOptions>
+): Promise<T> {
+    return {} as any
+}
+
+export function deserialize<T>(json: Uint8Array, meta: Metadata, options?: Partial<JsonOptions>): T {
     const opts = !!options ?
         optionsCache.getOrAdd(options, (key) => mergerOptions(defaultOptions, key)) :
         defaultOptions
 
-    const result = metadata.convert({
+    const result = meta.convert({
         bytes: json,
         options: opts
-    }, metadata, 0, 0)
+    }, meta, 0, 0)
 
     if (isError(result))
         throw new Error(result.error)
