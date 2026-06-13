53b623c9b691a79129d5a65d09ba42b167731bd0
User <ulyanovskiy.2001@mail.ru>
2026-06-13 06:07:37 +0300

    feat: create base util functions for specific numbers parsing

---
+++ src/utils/number.ts
@@ -0,0 +1,19 @@
+export function parseNumberI8(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberU8(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberI16(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberU16(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberI32(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberU32(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberF32(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberF64(b: Uint8Array, offset: number): number { return 1 }
+
+export function parseNumberI64(b: Uint8Array, offset: number): bigint { return 1n }
+
+export function parseNumberU64(b: Uint8Array, offset: number): bigint { return 1n }
