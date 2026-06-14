279350249f5095f02a48a35063f05eaea8626d08
User <ulyanovskiy.2001@mail.ru>
2026-06-14 15:51:38 +0300

    feat: update from string converter

--- src/converters/date.ts
+++ src/converters/date.ts
@@ -1,15 +1,16 @@
 import { BaseMeta, ConvertCtx, isObjectFieldMeta } from "../metadata/types"
+import { JsonOptions } from "../options"
 import { tryParseISO8601 } from "../utils/date"
-import { DOUBLE_QUOTE, isDigit } from "../utils/utf8constants"
+import { DOUBLE_QUOTE, isDigitU8 } from "../utils/utf8constants"
 
 export function toDate(ctx: ConvertCtx, meta: BaseMeta<Date>, index: number, _depth: number): Date {
     const i = index
     const b = ctx.bytes
 
     if (b[i] === DOUBLE_QUOTE) {
-        return fromString(b, i)
+        return fromString(b, i + 1, ctx.options)
     }
-    else if (isDigit(b[i])) {
+    else if (isDigitU8(b[i])) {
         return fromTimestamp(b, i)
     }
 
@@ -19,11 +20,18 @@ export function toDate(ctx: ConvertCtx, meta: BaseMeta<Date>, index: number, _de
     throw new Error(`invalid date value, at index ${i}`)
 }
 
-function fromString(b: Uint8Array, i: number): Date {
+function fromString(b: Uint8Array, i: number, opt: JsonOptions): Date {
     let milliseconds: number = 0
 
-    if ((milliseconds = tryParseISO8601(b, i)) > 0)
+    if ((milliseconds = tryParseISO8601(b, i)) >= 0)
         return new Date(milliseconds)
+
+    let start = i
+    while (i < b.length && b[i] !== DOUBLE_QUOTE) i++
+    const dateString = opt.decoder.decode(b.subarray(start, i))
+    const date = new Date(dateString)
+    if (!isNaN(date.valueOf()))
+        return date
 
     throw new Error(`invlid date format, at index ${i}`)
 }
