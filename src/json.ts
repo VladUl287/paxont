c5dc0069f6563b265bc46c9cce5b74c959072cc2
User <ulyanovskiy.2001@mail.ru>
2026-06-14 17:27:53 +0300

    feat: remove async deserializer temporary

--- src/json.ts
+++ src/json.ts
@@ -2,34 +2,15 @@ import { isError } from "./converters/types"
 import { Metadata } from "./metadata/metadata"
 import { defaultOptions, JsonOptions, mergeOptions } from "./options"
 import { createCache } from "./cache/cache"
-import { BaseMeta, ObjectMeta } from "./metadata/types"
+import { BaseMeta } from "./metadata/types"
 
 const optionsCache = createCache<Partial<JsonOptions>, JsonOptions>()
-
-// export function deserialize1<T, M extends BaseMeta<T> = BaseMeta<T>>(
-//     json: Uint8Array,
-//     meta: BaseMeta<T> | T,
-//     options?: Partial<JsonOptions>
-// ): T {
-//     return {} as any
-// }
 
 export function deserialize1<T, M extends BaseMeta<T> = BaseMeta<T>>(
     json: Uint8Array,
     meta: M | T,
     options?: Partial<JsonOptions>
 ): T {
-    return {} as any
-}
-
-deserialize1(new Uint8Array(), {} as ObjectMeta<{ id: number }>)
-deserialize1(new Uint8Array(), { id: 1 })
-
-export async function deserializeAsync<T, M extends BaseMeta<T> = BaseMeta<T>>(
-    stream: ReadableStream,
-    meta: M | T,
-    options?: Partial<JsonOptions>
-): Promise<T> {
     return {} as any
 }
 
