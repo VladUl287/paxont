bc3851e29b396b426b7867a86bda802cd592d32b
User <ulyanovskiy.2001@mail.ru>
2026-06-15 10:49:46 +0300

    feat: boolean converter return convert result

--- src/converters/boolean.ts
+++ src/converters/boolean.ts
@@ -1,16 +1,23 @@
 import { BaseMeta, ConvertCtx, isObjectFieldMeta } from "../metadata/types"
 import { A, E, F, L, R, S, T, U } from "../utils/utf8constants"
+import { ConvertResult } from "./types"
 
-export function toBoolean(ctx: ConvertCtx, meta: BaseMeta<boolean>, index: number, _depth: number): boolean {
+export function toBoolean(ctx: ConvertCtx, meta: BaseMeta<boolean>, index: number, _depth: number): ConvertResult<boolean> {
     const i = index
     const b = ctx.bytes
     const len = b.length
 
     if (i + 3 < len && b[i] === T && b[i + 1] === R && b[i + 2] === U && b[i + 3] === E)
-        return true
+        return {
+            value: true,
+            nextIndex: i + 4
+        }
 
     if (i + 4 < len && b[i] === F && b[i + 1] === A && b[i + 2] === L && b[i + 3] === S && b[i + 4] === E)
-        return false
+        return {
+            value: false,
+            nextIndex: i + 5
+        }
 
     if (isObjectFieldMeta(meta))
         throw new Error(`invalid boolean: field '${meta.name.value}', at index ${i}`)
