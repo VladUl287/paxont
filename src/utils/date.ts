8b1be3262ac00d847f411d31633a8f9758394215
User <ulyanovskiy.2001@mail.ru>
2026-06-16 15:42:04 +0300

    feat: todate conveter use custom utc func for faster compute

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -90,7 +90,7 @@ export function tryParseISO8601(b: Uint8Array, i: number, r: Date): number {
     }
 
     if (i < len1 && b[i] === Z) { //Z
-        r.setTime(Date.UTC(YYYY, MM, DD, HH, mm, ss, sss))
+        r.setTime(utc(YYYY, MM, DD, HH, mm, ss, sss))
         return i
     }
 
@@ -120,4 +120,27 @@ export function tryParseISO8601(b: Uint8Array, i: number, r: Date): number {
 
     r.setTime(Date.UTC(YYYY, MM, DD, HH - (ZHH * sign), mm - (zmm * sign), ss, sss))
     return i
+}
+
+const months = new Uint16Array([0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334])
+const monthsLeap = new Uint16Array([0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335])
+
+const MAX_YEAR = 4096
+const daysYears = new Uint32Array(MAX_YEAR)
+for (let y = 1970; y < MAX_YEAR; y++) {
+    const days = isLeap(y) ? 366 : 365
+    daysYears[y] = days + daysYears[y - 1]
+}
+
+function utc(year: number, month: number, day = 1, hours = 0, minutes = 0, seconds = 0, ms = 0) {
+    if (year < 1970 || year > MAX_YEAR)
+        return Date.UTC(year, month, day, hours, minutes, seconds, ms)
+
+    const leap = isLeap(year)
+    const days = daysYears[year - 1] + (leap ? monthsLeap[month] : months[month]) + (day - 1)
+    return days * 86400000 + hours * 3600000 + minutes * 60000 + seconds * 1000 + ms
+}
+
+function isLeap(y: number): boolean {
+    return (y % 4 === 0 && y % 100 !== 0) || (y % 400 === 0)
 }
