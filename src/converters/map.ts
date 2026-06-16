33e1f73b9dee9163549287bd23ed269007c220ea
User <ulyanovskiy.2001@mail.ru>
2026-06-16 18:41:05 +0300

    feat: add map converter support

---
+++ src/converters/map.ts
@@ -0,0 +1,47 @@
+import { ConvertCtx, MapMeta } from "../metadata/types"
+import { COLON, COMMA, CURLY_CLOSE, CURLY_OPEN, DOUBLE_QUOTE } from "../utils/utf8constants"
+import { ConvertResult } from "./types"
+import { skipWhitespace } from "./utils"
+
+export function toMap<V>(ctx: ConvertCtx, meta: MapMeta<V>, index: number, depth: number): ConvertResult<Map<string, V>> {
+    const b = ctx.bytes
+
+    if (b[index] !== CURLY_OPEN)
+        throw new Error(`object open not found at position ${index}. depth ${depth}`)
+
+    index++
+
+    const map = new Map<string, V>()
+
+    while (true) {
+        index = skipWhitespace(b, index)
+
+        if (b[index] !== DOUBLE_QUOTE)
+            throw new Error(`not start of property ${index}`)
+        index++
+
+        const key = meta.key.toValue(ctx, meta.key, index, depth)
+        index = key.nextIndex
+
+        if (b[index] !== COLON)
+            throw new Error(`not end of property`)
+        index++
+
+        index = skipWhitespace(b, index)
+
+        const value = meta.value.toValue(ctx, meta.value, index, depth)
+        index = value.nextIndex
+
+        map.set(key.value, value.value)
+
+        index = skipWhitespace(b, index)
+
+        if (b[index] === COMMA || b[index] === CURLY_CLOSE)
+            break
+    }
+
+    return {
+        value: map,
+        nextIndex: ++index
+    }
+}
