28cd4d2946778aec47629dc205c2409a590b6d25
User <ulyanovskiy.2001@mail.ru>
2026-06-17 12:05:28 +0300

    feat: create typed arrays converter

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,19 +1,48 @@
-export function parseNumberI8(b: Uint8Array, offset: number): number { return 1 }
+import { ConvertResult } from "../metadata/types"
 
-export function parseNumberU8(b: Uint8Array, offset: number): number { return 1 }
+type NumberFormat = {
+    normalMantissaBits: number
+    denormalMantissaBits: number
+    exponentBias: number
+    maxBinaryExponent: number
+    minBinaryExponent: number
+    exponentBits: number
+    normalMantissaMask: bigint
+    denormalMantissaMask: bigint
+    zeroBits: number
+    overflowDecimalExponent: number
+}
 
-export function parseNumberI16(b: Uint8Array, offset: number): number { return 1 }
+export const f64Format: NumberFormat = {
+    normalMantissaBits: 53,
+    denormalMantissaBits: 52,
+    exponentBias: 1023,
+    maxBinaryExponent: 1023,
+    minBinaryExponent: -1022,
+    exponentBits: 11,
+    normalMantissaMask: (1n << 53n) - 1n,
+    denormalMantissaMask: (1n << 52n) - 1n,
+    zeroBits: 0,
+    overflowDecimalExponent: 324
+}
 
-export function parseNumberU16(b: Uint8Array, offset: number): number { return 1 }
+export const f32Format: NumberFormat = {
+    normalMantissaBits: 53,
+    denormalMantissaBits: 52,
+    exponentBias: 1023,
+    maxBinaryExponent: 1023,
+    minBinaryExponent: -1022,
+    exponentBits: 11,
+    normalMantissaMask: (1n << 53n) - 1n,
+    denormalMantissaMask: (1n << 52n) - 1n,
+    zeroBits: 0,
+    overflowDecimalExponent: 324
+}
 
-export function parseNumberI32(b: Uint8Array, offset: number): number { return 1 }
+export function parseNumber(b: Uint8Array, offset: number, format: NumberFormat): ConvertResult<number> {
+    return {
+        value: 1,
+        nextIndex: 1
+    }
+}
 
-export function parseNumberU32(b: Uint8Array, offset: number): number { return 1 }
-
-export function parseNumberF32(b: Uint8Array, offset: number): number { return 1 }
-
-export function parseNumberF64(b: Uint8Array, offset: number): number { return 1 }
-
-export function parseNumberI64(b: Uint8Array, offset: number): bigint { return 1n }
-
-export function parseNumberU64(b: Uint8Array, offset: number): bigint { return 1n }
