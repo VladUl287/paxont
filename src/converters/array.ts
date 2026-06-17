34a07f9c3da69b2f0ae76275b38afa2c3d048bc5
User <ulyanovskiy.2001@mail.ru>
2026-06-17 09:58:31 +0300

    feat: add array converter

---
+++ src/converters/array.ts
@@ -0,0 +1,39 @@
+import { CollectionMeta, ConvertCtx, MapMeta } from "../metadata/types"
+import { COMMA, SQUARE_CLOSE, SQUARE_OPEN } from "../utils/utf8constants"
+import { ConvertResult } from "./types"
+import { skipWhitespace } from "./utils"
+
+export function toArray<V>(ctx: ConvertCtx, m: CollectionMeta<Array<V>, V>, i: number, d: number): ConvertResult<Array<V>> {
+    const b = ctx.bytes
+
+    if (b[i] !== SQUARE_OPEN)
+        throw new Error(`array open not found at position ${i}. depth ${d}`)
+    i++
+
+    const result = new Array<V>()
+    const mValue = m.value
+
+    let j = 0
+    while (true) {
+        i = skipWhitespace(b, i)
+
+        const arrayItemResult = mValue.toValue(ctx, mValue, i, d)
+        result[j] = arrayItemResult.value
+        i = arrayItemResult.nextIndex
+        j++
+
+        i = skipWhitespace(b, i)
+
+        if (b[i] === SQUARE_CLOSE)
+            break
+
+        if (b[i] !== COMMA)
+            throw new Error('not end of value')
+        i++
+    }
+
+    return {
+        value: result,
+        nextIndex: ++i
+    }
+}
