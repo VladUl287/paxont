3ef56f3490894ce62ad5e40f922b74fe1d3d320d
User <ulyanovskiy.2001@mail.ru>
2026-06-18 17:00:47 +0300

    fix: use new converterresult type

--- src/converters/number.ts
+++ src/converters/number.ts
@@ -1,6 +1,7 @@
+import { ConvertResult } from "../metadata/types"
 import { DOT, E, E_UPPER, MINUS, PLUS, ZERO } from "../utils/utf8constants"
 import { parseNumberF64_2 } from "./number_opt"
-import { ConvertMeta, ConvertResult, ConvertState } from "./types"
+import { ConvertMeta, ConvertState } from "./types"
 
 export function convertNumber(ctx: ConvertState, _metadata: ConvertMeta, index: number, _depth: number): ConvertResult<number> {
     return parseNumberF64_2(ctx.bytes, index)
