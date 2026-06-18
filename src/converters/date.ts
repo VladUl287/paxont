bc31d94de3e179c30768c1e4f51ae4165d278a01
User <ulyanovskiy.2001@mail.ru>
2026-06-18 16:48:09 +0300

    fix: convert date correspond to ISO 8601 Standard

--- src/converters/date.ts
+++ src/converters/date.ts
@@ -1,7 +1,6 @@
 import { BaseMeta, ConvertCtx, ConvertResult, isObjectFieldMeta } from "../metadata/types"
 import { JsonOptions } from "../options"
 import { tryParseISO8601 } from "../utils/date"
-import { parseNumberF64 } from "../utils/number"
 import { DOUBLE_QUOTE, isDigitU8 } from "../utils/utf8constants"
 
 export function toDate(ctx: ConvertCtx, meta: BaseMeta<Date>, index: number, _depth: number): ConvertResult<Date> {
@@ -24,33 +23,32 @@ export function toDate(ctx: ConvertCtx, meta: BaseMeta<Date>, index: number, _de
 }
 
 function fromString(b: Uint8Array, i: number, opt: JsonOptions): ConvertResult<Date> {
-    const result = new Date(0)
-    let index: number = 0
+    const result = {
+        value: Date.prototype,
+        nextIndex: 0
+    }
 
-    if ((index = tryParseISO8601(b, i, result)) >= 0)
-        return {
-            value: result,
-            nextIndex: index + 2
-        }
+    if (tryParseISO8601(b, i, result)) {
+        result.nextIndex += 2
+        return result
+    }
 
     let start = i
     while (i < b.length && b[i] !== DOUBLE_QUOTE) i++
 
     const dateStr = opt.decoder.decode(b.subarray(start, i))
-    const date = new Date(dateStr)
+    result.value = new Date(dateStr)
+    result.nextIndex = i
 
-    if (!isNaN(date.valueOf()))
-        return {
-            value: date,
-            nextIndex: i
-        }
+    if (!isNaN(result.value.valueOf()))
+        return result
 
     throw new Error(`invalid date value, at index ${i}`)
 }
 
 function fromTimestamp(b: Uint8Array, i: number): ConvertResult<Date> {
     return {
-        value: new Date(parseNumberF64(b, i)),
+        value: new Date(),
         nextIndex: i
     }
 }
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -20,106 +20,107 @@ function expectTwoDigits(b: Uint8Array, i: number): number {
     return ++i
 }
 
-export function tryParseISO8601(b: Uint8Array, i: number, r: Date): number {
+export function tryParseISO8601(b: Uint8Array, i: number, r: { value: Date, nextIndex: number }): boolean {
     const len1 = b.length - 1
 
     if ((i = expectFourDigits(b, i)) < 0) //YYYY
-        return -1
+        return false
 
     const YYYY = ((b[i - 4] & 0x0F) * 1000) + ((b[i - 3] & 0x0F) * 100) + ((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)
     if (i >= len1 || b[i++] !== MINUS) {
-        r.setTime(utc(YYYY, 0))
-        return i
+        r.value = new Date(YYYY, 0)
+        r.nextIndex = i
+        return true
     }
 
     if ((i = expectTwoDigits(b, i)) < 0) //MM
-        return -1
+        return false
 
     const MM = (((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)) - 1
     if (MM < 0 || MM > 11)
-        return -1
+        return false
 
     if (i >= len1 || b[i++] !== MINUS) {
-        r.setTime(utc(YYYY, MM))
-        return i
+        r.value = new Date(YYYY, MM)
+        r.nextIndex = i
+        return true
     }
 
     if ((i = expectTwoDigits(b, i)) < 0) //DD
-        return -1
+        return false
 
     const DD = ((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)
     if (DD < 1 || DD > 31)
-        return -1
+        return false
 
     if (i >= len1 || b[i++] !== T_UPPER) {
-        r.setTime(utc(YYYY, MM, DD))
-        return i
+        r.value = new Date(YYYY, MM, DD)
+        r.nextIndex = i
+        return true
     }
 
     if ((i = expectTwoDigits(b, i)) < 0 || b[i++] !== COLON || (i = expectTwoDigits(b, i)) < 0) //HH:mm
-        return -1
+        return false
 
     const HH = ((b[i - 5] & 0x0F) * 10) + (b[i - 4] & 0x0F)
     if (HH < 0 || HH > 23)
-        return -1
+        return false
 
     const mm = ((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)
     if (mm < 0 || mm > 59)
-        return -1
+        return false
 
     if (i >= len1 || b[i++] !== COLON) {
-        r.setTime(utc(YYYY, MM, DD, HH, mm))
-        return i
+        r.value = new Date(YYYY, MM, DD, HH, mm)
+        r.nextIndex = i
+        return true
     }
 
     if ((i = expectTwoDigits(b, i)) < 0) //ss
-        return -1
+        return false
 
     const ss = ((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)
     if (ss < 0 || ss > 59)
-        return -1
+        return false
 
     let sss = 0
     if (i <= len1 && b[i] === DOT) {
         if ((i = expectThreeDigits(b, i + 1)) < 0) //sss
-            return -1
+            return false
 
         sss = ((b[i - 3] & 0x0F) * 100) + ((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)
         if (sss < 0 || ss > 999)
-            return -1
+            return false
     }
 
     if (i < len1 && b[i] === Z) { //Z
-        r.setTime(utc(YYYY, MM, DD, HH, mm, ss, sss))
-        return i
+        r.value = new Date(utc(YYYY, MM, DD, HH, mm, ss, sss))
+        r.nextIndex = i
+        return true
     }
 
     if (i >= len1 && b[i] !== MINUS && b[i] !== PLUS) { //not ±
-        r.setFullYear(YYYY)
-        r.setMonth(MM)
-        r.setDate(DD)
-        r.setHours(HH)
-        r.setMinutes(mm)
-        r.setSeconds(ss)
-        r.setMilliseconds(sss)
-        return i
+        r.value = new Date(YYYY, MM, DD, HH, mm, ss, sss)
+        r.nextIndex = i
+        return true
     }
 
     const sign = b[i] === MINUS ? -1 : 1
 
     if ((i = expectTwoDigits(b, i)) < 0 || b[++i] !== COLON || (i = expectTwoDigits(b, i)) < 0) //HH:mm
-        return -1
+        return false
 
     const ZHH = ((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)
     if (ZHH < 0 || ZHH > 23)
-        return -1
+        return false
 
     const zmm = ((b[i - 2] & 0x0F) * 10) + (b[i - 1] & 0x0F)
     if (zmm < 0 || zmm > 59)
-        return -1
+        return false
 
-    r.setTime(utc(YYYY, MM, DD, HH - (ZHH * sign), mm - (zmm * sign), ss, sss))
-    return i
+    r.value = new Date(utc(YYYY, MM, DD, HH - (ZHH * sign), mm - (zmm * sign), ss, sss))
+    r.nextIndex = i
+    return true
 }
 
 const months = new Uint16Array([0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334])
