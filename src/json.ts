83c485922618886a3dde02ec2e9eae2c0c08b866
User <ulyanovskiy.2001@mail.ru>
2026-06-22 12:00:34 +0300

    feat: support json as array and as string

--- src/json.ts
+++ src/json.ts
@@ -5,12 +5,26 @@ import { BaseMeta } from "./metadata/types"
 
 const optionsCache = createCache<Partial<JsonOptions>, JsonOptions>()
 
-export function deserialize1<T, M extends BaseMeta<T> = BaseMeta<T>>(
+export function deserialize2<T, M extends BaseMeta<T> = BaseMeta<T>>(
     json: Uint8Array,
     meta: M | T,
     options?: Partial<JsonOptions>
 ): T {
     return {} as any
+}
+
+
+export function deserialize1<T>(json: string, meta: Metadata, options?: Partial<JsonOptions>): T {
+    const opts = !!options ?
+        optionsCache.getOrAdd(options, (key) => mergeOptions(defaultOptions, key)) :
+        defaultOptions
+
+    const result = meta.convert({
+        bytes: opts.encoder.encode(json),
+        options: opts
+    }, meta, 0, 0)
+
+    return result.value as T
 }
 
 export function deserialize<T>(json: Uint8Array, meta: Metadata, options?: Partial<JsonOptions>): T {
