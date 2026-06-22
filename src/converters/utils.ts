7f9e64fbe08566d8d235afc93552a27db52d9f72
User <ulyanovskiy.2001@mail.ru>
2026-06-22 15:51:05 +0300

    feat: whitespace skip, check length

--- src/converters/utils.ts
+++ src/converters/utils.ts
@@ -7,6 +7,6 @@ const WS_BITMASK =
 export const isWhitespace = (byte: number) => (WS_BITMASK >> byte) & 1
 
 export function skipWhitespace(bytes: Uint8Array, i: number): number {
-    while (isWhitespace(bytes[i])) i++
+    while (i < bytes.length && isWhitespace(bytes[i])) i++
     return i
 }
