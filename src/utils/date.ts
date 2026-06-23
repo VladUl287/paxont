f0cfcc0e7f89ff1086fafe1209afadfd12334b0c
User <ulyanovskiy.2001@mail.ru>
2026-06-23 15:34:55 +0300

    feat: make one array instead of two for date utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,30 +4,21 @@ const monthsLeap = new Uint16Array([0, 31, 60, 91, 121, 152, 182, 213, 244, 274,
 const MAX_YEAR = 4096
 const MIN_YEAR = 0
 
-const daysYears = new Uint32Array(MAX_YEAR)
-for (let y = 1970; y < MAX_YEAR; y++) {
-    const days = isLeap(y) ? 366 : 365
-    daysYears[y] = days + daysYears[y - 1]
-}
+const days = (y: number) => isLeap(y) ? 366 : 365
 
-const daysYearsBefore = new Uint32Array(1970)
-for (let y = 1969; y >= 0; y--) {
-    const days = isLeap(y) ? 366 : 365
-    daysYearsBefore[y] = days + (daysYearsBefore[y + 1] || 0)
-}
+const daysYearsNew = new Int32Array(MAX_YEAR)
+for (let i = 1969; i >= 0; i--)
+    daysYearsNew[i] = -(days(i) + (-daysYearsNew[i + 1]))
+
+for (let i = 1970; i < MAX_YEAR; i++)
+    daysYearsNew[i] = days(i) + Math.max(daysYearsNew[i - 1], 0)
 
 export function utc(year: number, month: number, day = 1, hours = 0, minutes = 0, seconds = 0, ms = 0) {
     if (year < MIN_YEAR || year > MAX_YEAR)
         return Date.UTC(year, month, day, hours, minutes, seconds, ms)
 
-    const leap = isLeap(year)
-
-    if (year < 1970) {
-        const days = -daysYearsBefore[year] + (leap ? monthsLeap[month] : months[month]) + (day - 1)
-        return days * 86400000 + hours * 3600000 + minutes * 60000 + seconds * 1000 + ms
-    }
-
-    const days = daysYears[year - 1] + (leap ? monthsLeap[month] : months[month]) + (day - 1)
+    const monthDays = isLeap(year) ? monthsLeap[month] : months[month]
+    const days = daysYearsNew[year - 1] + monthDays + (day - 1)
     return days * 86400000 + hours * 3600000 + minutes * 60000 + seconds * 1000 + ms
 }
 
