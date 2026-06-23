7d6428d36de642c342f8c93a600a4a15a43c6da0
User <ulyanovskiy.2001@mail.ru>
2026-06-23 14:12:22 +0300

    feat: use sub converters for parse integers

--- src/converters/typedArray.ts
+++ src/converters/typedArray.ts
@@ -1,5 +1,4 @@
-import { CollectionMeta, ConvertCtx, ConvertResult } from "../metadata/types"
-import { parseInt64, parseUint64, parseInt8, parseInt16, parseUint8, parseUint16, parseUint32, parseInt32, parseFloat64, parseFloat32 } from "../utils/number"
+import { CollectionMeta, ConvertCtx, ConvertResult, TypeName } from "../metadata/types"
 import { TypedArray, TypedArrayCtor } from "../utils/typedArray"
 import { COMMA, SQUARE_CLOSE, SQUARE_OPEN } from "../utils/utf8constants"
 import { skipWhitespace } from "./utils"
@@ -14,23 +13,40 @@ export function toTypedArray(
         throw new Error(`Maximum depth exceeded: limit ${ctx.options.maxDepth} at index ${i}`);
 
     const b = ctx.bytes
-    switch (m.type) {
-        case 'i8[]': return toArray(b, i, i8, parseInt8)
-        case 'u8[]': return toArray(b, i, u8, parseUint8)
 
-        case 'i16[]': return toArray(b, i, i16, parseInt16)
-        case 'u16[]': return toArray(b, i, u16, parseUint16)
+    if (b[i] !== SQUARE_OPEN)
+        throw new Error(`Expected '[' at index ${i}, but found '${b[i]}' while parsing array`)
+    i++
 
-        case 'i32[]': return toArray(b, i, i32, parseInt32)
-        case 'u32[]': return toArray(b, i, u32, parseUint32)
+    const factory = builder(m.type)
 
-        case 'u64[]': return toArray(b, i, u64, parseUint64)
-        case 'i64[]': return toArray(b, i, i64, parseInt64)
+    let temp = factory(Math.min(1024, b.length))
+    let tempLength = temp.length
 
-        case 'f32[]': return toArray(b, i, f32, parseFloat32)
-        case 'f64[]': return toArray(b, i, f64, parseFloat64)
+    const value = m.value
+    const toValue = m.value.toValue
 
-        default: throw new Error(`Unsupported TypedArray type '${m.type}' at index ${i}`);
+    let j = 0
+    while (b[i] !== SQUARE_CLOSE) {
+        i = skipWhitespace(b, i)
+
+        const result = toValue(ctx, value, i, d)
+        temp[j] = result.value
+        i = result.nextIndex
+        j++
+
+        if (j >= tempLength)
+            temp = factory(temp.length * 2, temp)
+
+        i = skipWhitespace(b, i)
+
+        if (b[i] === COMMA)
+            i++
+    }
+
+    return {
+        value: temp.slice(0, j),
+        nextIndex: ++i
     }
 }
 
@@ -48,6 +64,26 @@ const u64 = createFactory<BigUint64Array>(BigUint64Array)
 const f32 = createFactory<Float32Array>(Float32Array)
 const f64 = createFactory<Float64Array>(Float64Array)
 
+const builder = (type: TypeName): BufferFactory<TypedArray> => {
+    switch (type) {
+        case 'i8[]': i8
+        case 'u8[]': u8
+        case 'i16[]': i16
+        case 'u16[]': u16
+
+        case 'i32[]': i32
+        case 'u32[]': u32
+
+        case 'i64[]': i64
+        case 'u64[]': u64
+
+        case 'f32[]': f32
+        case 'f64[]': f64
+
+        default: throw new Error(`error`)
+    }
+}
+
 function createFactory<T extends TypedArray>(ctor: TypedArrayCtor<T>): BufferFactory<T> {
     let array: T | null = null
     return (length, source) => {
@@ -62,41 +98,3 @@ function createFactory<T extends TypedArray>(ctor: TypedArrayCtor<T>): BufferFac
         return array
     }
 }
-
-function toArray<T extends TypedArray>(
-    b: Uint8Array,
-    i: number,
-    factory: BufferFactory<T>,
-    parseValue: Parser<T>
-): ConvertResult<T> {
-    if (b[i] !== SQUARE_OPEN)
-        throw new Error(`Expected '[' at index ${i}, but found '${b[i]}' while parsing array`)
-    i++
-
-    let temp = factory(Math.min(b.length, 1024))
-    let tempLength = temp.length
-
-    let j = 0
-    while (b[i] !== SQUARE_CLOSE) {
-        i = skipWhitespace(b, i)
-
-        const result = parseValue(b, i)
-        temp[j] = result.value
-        i = result.nextIndex
-        j++
-
-        if (j >= tempLength)
-            temp = factory(temp.length * 2, temp)
-
-        i = skipWhitespace(b, i)
-
-        if (b[i] === COMMA)
-            i++
-    }
-
-    return {
-        value: temp.slice(0, j) as T,
-        nextIndex: ++i
-    }
-}
-
