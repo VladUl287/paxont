4e2103c9d3cb51b0610f2bed7996d588cdaa71f4
User <ulyanovskiy.2001@mail.ru>
2026-06-24 14:53:42 +0300

    feat: make field constructor accept any meta

--- src/metadata/builder.ts
+++ src/metadata/builder.ts
@@ -1,12 +1,10 @@
 import { toArray } from "../converters/toValue/array"
 import { toFloat64 } from "../converters/toValue/float"
-import { toInt } from "../converters/toValue/int"
 import { toString } from "../converters/toValue/string"
-import { TypedArray } from "../utils/typedArray"
-import { BaseMeta, CollectionMeta, ObjectFieldMeta, ObjectMeta, toValueConverter, TypeName } from "./types"
+import { BaseMeta, CollectionMeta, ObjectFieldMeta, ObjectMeta, toValueConverter } from "./types"
 
 type Expand<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
-
+type Extract<M> = M extends BaseMeta<infer U> ? U : never
 type ObjectFromMeta<T extends ObjectFieldMeta<any, any>[]> = Expand<{
     [E in T[number]as E['name']['value']]: E['toValue'] extends toValueConverter<infer U> ? U : never
 }>
@@ -23,41 +21,9 @@ const number = (): BaseMeta<number> => ({
     toJson: (s, _m) => s.toString()
 })
 
-const array = <T>(value: BaseMeta<T>): CollectionMeta<Array<T>, T> => ({
-    type: 'array',
-    toValue: toArray as any,
-    toJson: (s, _m) => `[${s.join(',')}]`,
-    value: value
-})
-
-const u8 = (): BaseMeta<number> => ({
-    type: 'u8',
-    toValue: toInt,
-    toJson: (s, _m) => s.toString()
-})
-
-const u16 = (): BaseMeta<number> => ({
-    type: 'u16',
-    toValue: toInt,
-    toJson: (s, _m) => s.toString()
-})
-
-const u32 = (): BaseMeta<number> => ({
-    type: 'u32',
-    toValue: toInt,
-    toJson: (s, _m) => s.toString()
-})
-
-const u8Array = (): CollectionMeta<Uint8Array, number> => typedArray('u8[]', u8())
-
-const typedArray = <T extends TypedArray>(type: TypeName, value: BaseMeta<number>): CollectionMeta<T, number> => ({
-    type: type,
-    toValue: toArray as any,
-    toJson: (s, _m) => `[${s.join(',')}]`,
-    value: value
-})
-
-const field = <K extends string, T>(name: K, value: BaseMeta<T>): ObjectFieldMeta<K, T> => {
+const field = <K extends string, M extends BaseMeta<any>>(
+    name: K, value: M
+): ObjectFieldMeta<K, Extract<M>> => {
     return {} as any
 }
 
@@ -68,8 +34,6 @@ export const object = <M extends ObjectFieldMeta<any, any>[]>(...fields: M): Obj
 const obj = object(
     field("id", number()),
     field("name", string()),
-    field("tags", array(string())),
-    field("deltas", u8Array()),
     field("role", object(
         field("id", number()),
         field("value", string())
