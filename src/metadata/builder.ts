f44d8a3c40e3d8ff649ff88c2ed428ba23b9729a
User <ulyanovskiy.2001@mail.ru>
2026-06-24 13:58:20 +0300

    feat: meta builder types add array support

--- src/metadata/builder.ts
+++ src/metadata/builder.ts
@@ -1,6 +1,9 @@
+import { toArray } from "../converters/toValue/array"
 import { toFloat64 } from "../converters/toValue/float"
+import { toInt } from "../converters/toValue/int"
 import { toString } from "../converters/toValue/string"
-import { BaseMeta, ObjectFieldMeta, ObjectMeta, toValueConverter } from "./types"
+import { TypedArray } from "../utils/typedArray"
+import { BaseMeta, CollectionMeta, ObjectFieldMeta, ObjectMeta, toValueConverter, TypeName } from "./types"
 
 type Expand<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
 
@@ -20,6 +23,40 @@ const number = (): BaseMeta<number> => ({
     toJson: (s, _m) => s.toString()
 })
 
+const array = <T>(value: BaseMeta<T>): CollectionMeta<Array<T>, T> => ({
+    type: 'array',
+    toValue: toArray as any,
+    toJson: (s, _m) => `[${s.join(',')}]`,
+    value: value
+})
+
+const u8 = (): BaseMeta<number> => ({
+    type: 'u8',
+    toValue: toInt,
+    toJson: (s, _m) => s.toString()
+})
+
+const u16 = (): BaseMeta<number> => ({
+    type: 'u16',
+    toValue: toInt,
+    toJson: (s, _m) => s.toString()
+})
+
+const u32 = (): BaseMeta<number> => ({
+    type: 'u32',
+    toValue: toInt,
+    toJson: (s, _m) => s.toString()
+})
+
+const u8Array = (): CollectionMeta<Uint8Array, number> => typedArray('u8[]', u8())
+
+const typedArray = <T extends TypedArray>(type: TypeName, value: BaseMeta<number>): CollectionMeta<T, number> => ({
+    type: type,
+    toValue: toArray as any,
+    toJson: (s, _m) => `[${s.join(',')}]`,
+    value: value
+})
+
 const field = <K extends string, T>(name: K, value: BaseMeta<T>): ObjectFieldMeta<K, T> => {
     return {} as any
 }
@@ -31,6 +68,8 @@ export const object = <M extends ObjectFieldMeta<any, any>[]>(...fields: M): Obj
 const obj = object(
     field("id", number()),
     field("name", string()),
+    field("tags", array(string())),
+    field("deltas", u8Array()),
     field("role", object(
         field("id", number()),
         field("value", string())
