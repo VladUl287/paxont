f2acdd297a590cbc6af851b2cad90d307eca75e7
User <ulyanovskiy.2001@mail.ru>
2026-06-25 15:22:16 +0300

    feat: remove old unused metadata types

--- src/converters/types.ts
+++ src/converters/types.ts
@@ -1,10 +1,3 @@
 import { Metadata } from "../metadata/metadata"
-import { ConvertCtx } from "../metadata/types"
-import { ReadResult } from "../utils/types"
 
 export type ConvertMeta = Metadata | Metadata[]
-
-export const isSingleMeta = (meta: ConvertMeta): meta is Metadata => !Array.isArray(meta)
-export const isMultiMeta = (meta: ConvertMeta): meta is Metadata[] => Array.isArray(meta)
-
-export type Converter<T> = (ctx: ConvertCtx, meta: ConvertMeta, index: number, depth: number) => ReadResult<T>
