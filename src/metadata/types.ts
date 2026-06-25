d3eb76f89082537f50f5ac867a63554cb4e661eb
User <ulyanovskiy.2001@mail.ru>
2026-06-25 14:52:59 +0300

    feat: add isMeta util to check type

--- src/metadata/types.ts
+++ src/metadata/types.ts
@@ -177,3 +177,17 @@ export function useMetadata(): UseMetadata {
 
 //     return hasNameValue && hasNameBytes
 // }
+
+
+export function isMeta(value: unknown): value is BaseMeta<any, any> {
+    if (!value || typeof value !== 'object')
+        return false
+
+    const potential = value as Record<string, unknown>
+
+    const hasToValue = typeof potential.toValue === 'function'
+    const hasToJson = typeof potential.toJson === 'function'
+    const hasType = typeof potential.type === 'string'
+
+    return hasToValue && hasToJson && hasType
+}
