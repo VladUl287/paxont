5dc8b74943d60e26483fa609e915debc77a3a2ca
User <ulyanovskiy.2001@mail.ru>
2026-06-25 16:51:33 +0300

    feat: create wrappers for specific types to support all meta logic in meta from object builder

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,22 @@ export type ReadResult<T> = {
     readonly value: T
     readonly nextIndex: number
 }
+
+export abstract class BaseWrapper<T> {
+    constructor(private readonly _value: T) { }
+
+    public get value() {
+        return this._value
+    }
+}
+
+export class Nullable<T> extends BaseWrapper<T> { }
+export class Int8 extends BaseWrapper<number> { }
+export class Int16 extends BaseWrapper<number> { }
+export class Int32 extends BaseWrapper<number> { }
+export class Int64 extends BaseWrapper<number> { }
+export class Uint8 extends BaseWrapper<number> { }
+export class Uint16 extends BaseWrapper<number> { }
+export class Uint32 extends BaseWrapper<number> { }
+export class Uint64 extends BaseWrapper<number> { }
+export class Float32 extends BaseWrapper<number> { }
