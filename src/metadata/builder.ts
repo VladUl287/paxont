a98a97c9e99523677212153ba426235f4e4989e7
User <ulyanovskiy.2001@mail.ru>
2026-06-25 10:05:28 +0300

    feat: revert meta to interfaces

--- src/metadata/builder.ts
+++ src/metadata/builder.ts
@@ -1,7 +1,9 @@
 import { toArray } from "../converters/toValue/array"
 import { toFloat64 } from "../converters/toValue/float"
+import { toInt } from "../converters/toValue/int"
 import { toString } from "../converters/toValue/string"
-import { BaseMeta, CollectionMeta, ObjectFieldMeta, ObjectMeta, toValueConverter } from "./types"
+import { TypedArray } from "../utils/typedArray"
+import { BaseMeta, CollectionMeta, ObjectFieldMeta, ObjectMeta, PrimitiveMeta, toValueConverter, TypeName } from "./types"
 
 type Expand<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
 type Extract<M> = M extends BaseMeta<infer U, any> ? U : never
@@ -9,13 +11,13 @@ type ObjectFromMeta<T extends ObjectFieldMeta<any, any>[]> = Expand<{
     [E in T[number]as E['name']['value']]: E['toValue'] extends toValueConverter<infer U, any> ? U : never
 }>
 
-const string = (): BaseMeta<string> => ({
+const string = (): PrimitiveMeta<string> => ({
     type: 'string',
     toValue: toString,
     toJson: (s, _) => s
 })
 
-const number = (): BaseMeta<number> => ({
+const number = (): PrimitiveMeta<number> => ({
     type: 'number',
     toValue: toFloat64,
     toJson: (s, _) => s.toString()
@@ -23,6 +25,21 @@ const number = (): BaseMeta<number> => ({
 
 const array = <T extends BaseMeta<any, any>>(value: T): CollectionMeta<Array<Extract<T>>, Extract<T>> => ({
     type: 'array',
+    toValue: toArray,
+    toJson: (s, _) => `[${s.join(',')}]`,
+    value: value
+})
+
+const u8 = (): PrimitiveMeta<number> => ({
+    type: 'u8',
+    toValue: toInt,
+    toJson: (s, _) => s.toString()
+})
+
+const arrayU8 = (): CollectionMeta<Uint8Array, number> => typedArray('u8[]', u8())
+
+const typedArray = <T extends TypedArray>(type: TypeName, value: PrimitiveMeta<number>): CollectionMeta<T, number> => ({
+    type: type,
     toValue: toArray,
     toJson: (s, _) => `[${s.join(',')}]`,
     value: value
--- src/metadata/types.ts
+++ src/metadata/types.ts
@@ -1,6 +1,4 @@
 import { JsonOptions } from "../options"
-import { isPlainObject } from "../utils/object"
-import { isTypedArray } from "../utils/typedArray"
 import { ReadResult } from "../utils/types"
 
 export type BuiltInType =
@@ -20,206 +18,209 @@ export type ConvertCtx = {
     readonly options: JsonOptions
 }
 
-export type toValueConverter<T, M extends BaseMeta<T, any>> =
+export type toValueConverter<T, M extends BaseMeta<T, M>> =
     (ctx: ConvertCtx, meta: M, index: number, depth: number) => ReadResult<T>
-export type toJsonConverter<T> = (value: T, meta: BaseMeta<T>) => string
 
-export interface BaseMeta<T, M extends BaseMeta<T, M> = BaseMeta<T, any>> {
+export type toJsonConverter<T> = (value: T, meta: BaseMeta<T, any>) => string
+
+export interface BaseMeta<T, M extends BaseMeta<T, M>> {
     readonly toValue: toValueConverter<T, M>,
     readonly toJson: toJsonConverter<T>,
     readonly type: TypeName
+}
+
+export interface PrimitiveMeta<T> extends BaseMeta<T, PrimitiveMeta<T>> { }
+
+export interface ObjectMeta<T> extends BaseMeta<T, ObjectMeta<T>> {
+    readonly fields: ObjectFields<T>
+    readonly factory: (values: T[keyof T][]) => T
+    readonly fieldIndexResolver: (field: Uint8Array, index: number) => number
 }
 
 export type ObjectFields<T> = {
     [K in keyof T]: ObjectFieldMeta<T[K], K>
 }[keyof T][]
 
-export type ObjectMeta<T> = BaseMeta<T, ObjectMeta<T>> & {
-    readonly fields: ObjectFields<T>
-    readonly factory: (values: T[keyof T][]) => T
-    readonly fieldIndexResolver: (field: Uint8Array, index: number) => number
-}
-
-export type ObjectFieldMeta<K, T> = BaseMeta<T> & {
+export type ObjectFieldMeta<K, T> = BaseMeta<T, ObjectFieldMeta<K, T>> & {
     readonly name: {
         value: K
         bytes: Uint8Array
     }
 }
 
-export type CollectionMeta<T, V> = BaseMeta<T, CollectionMeta<T, V>> & {
-    readonly value: BaseMeta<V>
+export interface CollectionMeta<T, V> extends BaseMeta<T, CollectionMeta<T, V>> {
+    readonly value: BaseMeta<V, any>
 }
 
-export interface MapMeta<V> extends BaseMeta<Map<string, V>> {
-    readonly key: BaseMeta<string>
-    readonly value: BaseMeta<V>
+export interface MapMeta<V> extends BaseMeta<Map<string, V>, MapMeta<V>> {
+    readonly key: BaseMeta<string, any>
+    readonly value: BaseMeta<V, any>
 }
 
-export type TypeChecker<T = any> = (data: any) => data is T
-export type TypeProcessor<T = any, R = BaseMeta<T>> = (data: T) => R
+// export type TypeChecker<T = any> = (data: any) => data is T
+// export type TypeProcessor<T = any, R = BaseMeta<T, any>> = (data: T) => R
 
-export interface Type<T = any, R = BaseMeta<T>> {
-    name: TypeName
-    check: TypeChecker<T>
-    process: TypeProcessor<T, R>
-    priority: number
-}
+// export interface Type<T = any, R = BaseMeta<T, any>> {
+//     name: TypeName
+//     check: TypeChecker<T>
+//     process: TypeProcessor<T, R>
+//     priority: number
+// }
 
-export type UseMetadata = {
-    addType: <T, R extends BaseMeta<T>>(type: Type<T, R>) => void
-    removeType: (type: string | Type) => Type
-    getTypes: () => Type[]
-    clearTypes: () => void
-    hasType: (name: string) => boolean
-    toMetadata: <T>(data: T) => BaseMeta<T>
-}
+// export type UseMetadata = {
+//     addType: <T, R extends BaseMeta<T, any>>(type: Type<T, R>) => void
+//     removeType: (type: string | Type) => Type
+//     getTypes: () => Type[]
+//     clearTypes: () => void
+//     hasType: (name: string) => boolean
+//     toMetadata: <T>(data: T) => BaseMeta<T, any>
+// }
 
-export function useMetadata(): UseMetadata {
-    const types = new Map<string, Type>()
+// export function useMetadata(): UseMetadata {
+//     const types = new Map<string, Type>()
 
-    const withDefaultTypes = (metadata: UseMetadata): UseMetadata => {
-        metadata.addType<object, ObjectMeta<object>>({
-            name: 'object',
-            check: (data): data is object => isPlainObject(data),
-            process: (data) => ({} as any),
-            priority: 50
-        })
-        metadata.addType<any[], CollectionMeta<any[], any>>({
-            name: 'array',
-            check: (data): data is any[] => Array.isArray(data),
-            process: (data) => {
-                const collectionItem = metadata.toMetadata(data[0])
-                return {
-                    type: 'array',
-                    value: collectionItem,
-                    toJson: {} as any,
-                    toValue: {} as any,
-                }
-            },
-            priority: 50
-        })
-        metadata.addType<Date, BaseMeta<Date>>({
-            name: 'date',
-            check: (data): data is Date => data instanceof Date,
-            process: (date) => {
-                return {
-                    type: 'date',
-                    toJson: {} as any,
-                    toValue: {} as any,
-                }
-            },
-            priority: 51
-        })
-        metadata.addType<number, BaseMeta<number>>({
-            name: 'number',
-            check: (data): data is number => typeof data === 'number',
-            process: (data) => {
-                return {
-                    type: 'number',
-                    toJson: {} as any,
-                    toValue: {} as any,
-                }
-            },
-            priority: 51
-        })
-        return metadata
-    }
+//     const withDefaultTypes = (metadata: UseMetadata): UseMetadata => {
+//         metadata.addType<object, ObjectMeta<object>>({
+//             name: 'object',
+//             check: (data): data is object => isPlainObject(data),
+//             process: (data) => ({} as any),
+//             priority: 50
+//         })
+//         metadata.addType<any[], CollectionMeta<any[], any>>({
+//             name: 'array',
+//             check: (data): data is any[] => Array.isArray(data),
+//             process: (data) => {
+//                 const collectionItem = metadata.toMetadata(data[0])
+//                 return {
+//                     type: 'array',
+//                     value: collectionItem,
+//                     toJson: {} as any,
+//                     toValue: {} as any,
+//                 }
+//             },
+//             priority: 50
+//         })
+//         metadata.addType<Date, BaseMeta<Date>>({
+//             name: 'date',
+//             check: (data): data is Date => data instanceof Date,
+//             process: (date) => {
+//                 return {
+//                     type: 'date',
+//                     toJson: {} as any,
+//                     toValue: {} as any,
+//                 }
+//             },
+//             priority: 51
+//         })
+//         metadata.addType<number, BaseMeta<number>>({
+//             name: 'number',
+//             check: (data): data is number => typeof data === 'number',
+//             process: (data) => {
+//                 return {
+//                     type: 'number',
+//                     toJson: {} as any,
+//                     toValue: {} as any,
+//                 }
+//             },
+//             priority: 51
+//         })
+//         return metadata
+//     }
 
-    const addType = <T, R extends BaseMeta<T>>(type: Type<T, R>): void => { types.set(type.name, type) }
+//     const addType = <T, R extends BaseMeta<T>>(type: Type<T, R>): void => { types.set(type.name, type) }
 
-    const removeType = (type: string | Type): Type => {
-        const typeToDelete = typeof type === 'string' ? type : type.name
+//     const removeType = (type: string | Type): Type => {
+//         const typeToDelete = typeof type === 'string' ? type : type.name
 
-        const toDelete = types.get(typeToDelete)
-        if (!toDelete)
-            throw new Error()
+//         const toDelete = types.get(typeToDelete)
+//         if (!toDelete)
+//             throw new Error()
 
-        if (!types.delete(typeToDelete))
-            throw new Error()
+//         if (!types.delete(typeToDelete))
+//             throw new Error()
 
-        return toDelete
-    }
+//         return toDelete
+//     }
 
-    const hasType = (name: string): boolean => types.has(name)
+//     const hasType = (name: string): boolean => types.has(name)
 
-    const getTypes = (): Type[] => Array.from(types.values()).sort((a, b) => a.priority - b.priority)
+//     const getTypes = (): Type[] => Array.from(types.values()).sort((a, b) => a.priority - b.priority)
 
-    const clearTypes = (): void => types.clear()
+//     const clearTypes = (): void => types.clear()
 
-    const toMetadata = <T>(data: T): BaseMeta<T> => {
-        const types = getTypes()
+//     const toMetadata = <T>(data: T): BaseMeta<T> => {
+//         const types = getTypes()
 
-        for (const type of types) {
-            if (type.check(data))
-                return type.process(data)
-        }
+//         for (const type of types) {
+//             if (type.check(data))
+//                 return type.process(data)
+//         }
 
-        throw new Error(`No type handler found for: ${typeof data}`)
-    }
+//         throw new Error(`No type handler found for: ${typeof data}`)
+//     }
 
-    return withDefaultTypes({
-        addType,
-        removeType,
-        getTypes,
-        clearTypes,
-        hasType,
-        toMetadata
-    })
-}
+//     return withDefaultTypes({
+//         addType,
+//         removeType,
+//         getTypes,
+//         clearTypes,
+//         hasType,
+//         toMetadata
+//     })
+// }
 
-export function toMeta<T>(data: T): BaseMeta<T> {
-    if (data === null || data === undefined)
-        throw new Error('fail to detect type of data')
+// export function toMeta<T>(data: T): BaseMeta<T> {
+//     if (data === null || data === undefined)
+//         throw new Error('fail to detect type of data')
 
-    if (Array.isArray(data)) {
+//     if (Array.isArray(data)) {
 
-    }
+//     }
 
-    if (data instanceof Date) {
+//     if (data instanceof Date) {
 
-    }
+//     }
 
-    if (isTypedArray(data)) {
+//     if (isTypedArray(data)) {
 
-    }
+//     }
 
-    if (data instanceof Map) {
+//     if (data instanceof Map) {
 
-    }
+//     }
 
-    if (data instanceof Set) {
+//     if (data instanceof Set) {
 
-    }
+//     }
 
-    if (typeof data === "object") {
+//     if (typeof data === "object") {
 
-    }
+//     }
 
-    return {} as any
-}
+//     return {} as any
+// }
 
-export function toMeta1<T>(data: T, meta: BaseMeta<any>): BaseMeta<T> {
-    return {} as any
-}
+// export function toMeta1<T>(data: T, meta: BaseMeta<any>): BaseMeta<T> {
+//     return {} as any
+// }
 
-export function isObjectFieldMeta(obj: unknown): obj is ObjectFieldMeta<any, any> {
-    if (!obj || typeof obj !== 'object')
-        return false
+// export function isObjectFieldMeta(obj: unknown): obj is ObjectFieldMeta<any, any> {
+//     if (!obj || typeof obj !== 'object')
+//         return false
 
-    const potential = obj as Record<string, unknown>
+//     const potential = obj as Record<string, unknown>
 
-    const hasToValue = typeof potential.toValue === 'function'
-    const hasToJson = typeof potential.toJson === 'function'
-    const hasType = typeof potential.type === 'string'
+//     const hasToValue = typeof potential.toValue === 'function'
+//     const hasToJson = typeof potential.toJson === 'function'
+//     const hasType = typeof potential.type === 'string'
 
-    const hasName = potential.name !== undefined && potential.name !== null && typeof potential.name === 'object'
+//     const hasName = potential.name !== undefined && potential.name !== null && typeof potential.name === 'object'
 
-    if (!hasToValue || !hasToJson || !hasType || !hasName) return false
+//     if (!hasToValue || !hasToJson || !hasType || !hasName) return false
 
-    const nameObj = potential.name as Record<string, unknown>
-    const hasNameValue = 'value' in nameObj
-    const hasNameBytes = nameObj.bytes instanceof Uint8Array
+//     const nameObj = potential.name as Record<string, unknown>
+//     const hasNameValue = 'value' in nameObj
+//     const hasNameBytes = nameObj.bytes instanceof Uint8Array
 
-    return hasNameValue && hasNameBytes
-}
+//     return hasNameValue && hasNameBytes
+// }
