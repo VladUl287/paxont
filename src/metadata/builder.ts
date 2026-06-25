5832ab6d1d4ef9a0ccfc8fc446765f621a9e7923
User <ulyanovskiy.2001@mail.ru>
2026-06-25 11:05:24 +0300

    feat: use types constants

--- src/metadata/builder.ts
+++ src/metadata/builder.ts
@@ -5,7 +5,7 @@ import { toInt } from "../converters/toValue/int"
 import { toString } from "../converters/toValue/string"
 import { TypedArray } from "../utils/typedArray"
 import { BaseMeta, CollectionMeta, ObjectFieldMeta, ObjectMeta, PrimitiveMeta, toValueConverter, TypeName } from "./types"
-import { JSONT_BOOL, JSONT_I16, JSONT_I16_ARRAY, JSONT_I32, JSONT_I32_ARRAY, JSONT_I8, JSONT_I8_ARRAY, JSONT_U16, JSONT_U16_ARRAY, JSONT_U32, JSONT_U32_ARRAY, JSONT_U8, JSONT_U8_ARRAY } from "./baseTypes"
+import * as Base from "./baseTypes"
 import { toBoolean } from "../converters/toValue/boolean"
 
 type Expand<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
@@ -15,35 +15,35 @@ type ObjectFromMeta<T extends ObjectFieldMeta<any, any>[]> = Expand<{
 }>
 
 const string = (): PrimitiveMeta<string> => ({
-    type: 'string',
+    type: Base.JSONT_STRING,
     toValue: toString,
     toJson: (s, _) => s
 })
 
 const number = (): PrimitiveMeta<number> => ({
-    type: 'number',
+    type: Base.JSONT_NUMBER,
     toValue: toFloat64,
     toJson: (s, _) => s.toString()
 })
 
 const bigInt = (): PrimitiveMeta<bigint> => ({
-    type: 'bigint',
+    type: Base.JSONT_BIGINT,
     toValue: toBigInt,
     toJson: (s, _) => s.toString()
 })
 
 const bool = (): PrimitiveMeta<boolean> => ({
-    type: JSONT_BOOL,
+    type: Base.JSONT_BOOL,
     toValue: toBoolean,
     toJson: (s, _) => s.toString()
 })
 
-const u8 = (): PrimitiveMeta<number> => integer(JSONT_U8)
-const u16 = (): PrimitiveMeta<number> => integer(JSONT_U16)
-const u32 = (): PrimitiveMeta<number> => integer(JSONT_U32)
-const i8 = (): PrimitiveMeta<number> => integer(JSONT_I8)
-const i16 = (): PrimitiveMeta<number> => integer(JSONT_I16)
-const i32 = (): PrimitiveMeta<number> => integer(JSONT_I32)
+const u8 = (): PrimitiveMeta<number> => integer(Base.JSONT_U8)
+const u16 = (): PrimitiveMeta<number> => integer(Base.JSONT_U16)
+const u32 = (): PrimitiveMeta<number> => integer(Base.JSONT_U32)
+const i8 = (): PrimitiveMeta<number> => integer(Base.JSONT_I8)
+const i16 = (): PrimitiveMeta<number> => integer(Base.JSONT_I16)
+const i32 = (): PrimitiveMeta<number> => integer(Base.JSONT_I32)
 
 const integer = (type: TypeName): PrimitiveMeta<number> => ({
     type: type,
@@ -58,13 +58,13 @@ const array = <T extends BaseMeta<any, any>>(value: T): CollectionMeta<Array<Ext
     value: value
 })
 
-const u8Array = (): CollectionMeta<Uint8Array, number> => typedArray(JSONT_U8_ARRAY, u8())
-const u16Array = (): CollectionMeta<Uint16Array, number> => typedArray(JSONT_U16_ARRAY, u16())
-const u32Array = (): CollectionMeta<Uint32Array, number> => typedArray(JSONT_U32_ARRAY, u32())
+const u8Array = (): CollectionMeta<Uint8Array, number> => typedArray(Base.JSONT_U8_ARRAY, u8())
+const u16Array = (): CollectionMeta<Uint16Array, number> => typedArray(Base.JSONT_U16_ARRAY, u16())
+const u32Array = (): CollectionMeta<Uint32Array, number> => typedArray(Base.JSONT_U32_ARRAY, u32())
 
-const i8Array = (): CollectionMeta<Int8Array, number> => typedArray(JSONT_I8_ARRAY, i8())
-const i16Array = (): CollectionMeta<Int16Array, number> => typedArray(JSONT_I16_ARRAY, i16())
-const i32Array = (): CollectionMeta<Int32Array, number> => typedArray(JSONT_I32_ARRAY, i32())
+const i8Array = (): CollectionMeta<Int8Array, number> => typedArray(Base.JSONT_I8_ARRAY, i8())
+const i16Array = (): CollectionMeta<Int16Array, number> => typedArray(Base.JSONT_I16_ARRAY, i16())
+const i32Array = (): CollectionMeta<Int32Array, number> => typedArray(Base.JSONT_I32_ARRAY, i32())
 
 const typedArray = <T extends TypedArray>(type: TypeName, value: PrimitiveMeta<number>): CollectionMeta<T, number> => ({
     type: type,
@@ -87,6 +87,7 @@ const obj = object(
     field("id", number()),
     field("name", string()),
     field("deleted", bool()),
+    field("mantissa", bigInt()),
     field("timestamps", u32Array()),
     field("coordinates", array(
         object(
