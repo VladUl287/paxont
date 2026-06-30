131f8484767a7899e2f82e3a62a81f68c47f419d
User <ulyanovskiy.2001@mail.ru>
2026-06-30 16:38:59 +0300

    feat: add all types to default metadata support

--- src/metadata/types.ts
+++ src/metadata/types.ts
@@ -1,8 +1,8 @@
 import { JsonOptions } from "../options"
 import { isPlainObject } from "../utils/object"
-import { ReadResult } from "../utils/types"
+import { Int16, Int32, Int64, Int8, Nullable, ReadResult, Uint16, Uint32, Uint64, Uint8 } from "../utils/types"
 import * as Base from "./baseTypes"
-import { ExtractType, field, object } from "./builder"
+import { array, bigInt, bool, date, ExtractType, field, i16, i16Array, i32, i32Array, i64, i64Array, i8, i8Array, map, nullable, number, object, set, string, u16, u16Array, u32, u32Array, u64, u64Array, u8, u8Array } from "./builder"
 
 export type TypeName = Base.BaseTypes | (string & {})
 
@@ -78,15 +78,40 @@ export function useMetadata(): UseMetadata {
     const types = new Map<TypeName, JType>()
 
     const withDefaultTypes = (meta: UseMetadata): UseMetadata => {
-        meta.addType<ObjectMeta<object>>({
-            type: Base.JSONT_OBJECT,
-            check: (d): d is object => isPlainObject(d),
-            toMeta: (d) => object(
-                ...Object.entries(d)
-                    .map((c) => field(c[0], meta.toMetadata(c[1])))
-            ),
-            priority: 100
-        })
+        const types: JType<BaseMeta<any, any>>[] = [
+            { type: Base.JSONT_STRING, check: (d) => typeof d === 'string', toMeta: string, priority: 50 },
+            { type: Base.JSONT_NUMBER, check: (d) => typeof d === 'number', toMeta: number, priority: 50 },
+            { type: Base.JSONT_BIGINT, check: (d) => typeof d === 'bigint', toMeta: bigInt, priority: 50 },
+            { type: Base.JSONT_BOOL, check: (d) => typeof d === 'boolean', toMeta: bool, priority: 50 },
+            { type: Base.JSONT_DATE, check: (d) => d instanceof Date, toMeta: date, priority: 50 },
+            { type: Base.JSONT_ARRAY, check: (d) => Array.isArray(d), toMeta: (v) => array(meta.toMetadata(v)), priority: 50 },
+            { type: Base.JSONT_SET, check: (d) => d instanceof Set, toMeta: (v) => set(meta.toMetadata(v)), priority: 50 },
+            { type: Base.JSONT_MAP, check: (d) => d instanceof Map, toMeta: (v) => map(meta.toMetadata(v)), priority: 50 },
+            { type: Base.JSONT_NULLABLE, check: (d) => d instanceof Nullable, toMeta: (v) => nullable(meta.toMetadata(v)), priority: 50 },
+            { type: Base.JSONT_I8, check: (d) => d instanceof Int8, toMeta: i8, priority: 50 },
+            { type: Base.JSONT_I16, check: (d) => d instanceof Int16, toMeta: i16, priority: 50 },
+            { type: Base.JSONT_I32, check: (d) => d instanceof Int32, toMeta: i32, priority: 50 },
+            { type: Base.JSONT_I64, check: (d) => d instanceof Int64, toMeta: i64, priority: 50 },
+            { type: Base.JSONT_U8, check: (d) => d instanceof Uint8, toMeta: u8, priority: 50 },
+            { type: Base.JSONT_U16, check: (d) => d instanceof Uint16, toMeta: u16, priority: 50 },
+            { type: Base.JSONT_U32, check: (d) => d instanceof Uint32, toMeta: u32, priority: 50 },
+            { type: Base.JSONT_U64, check: (d) => d instanceof Uint64, toMeta: u64, priority: 50 },
+            { type: Base.JSONT_I8_ARRAY, check: (d) => d instanceof Int8Array, toMeta: i8Array, priority: 50 },
+            { type: Base.JSONT_I16_ARRAY, check: (d) => d instanceof Int16Array, toMeta: i16Array, priority: 50 },
+            { type: Base.JSONT_I32_ARRAY, check: (d) => d instanceof Int32Array, toMeta: i32Array, priority: 50 },
+            { type: Base.JSONT_I64_ARRAY, check: (d) => d instanceof BigInt64Array, toMeta: i64Array, priority: 50 },
+            { type: Base.JSONT_U8_ARRAY, check: (d) => d instanceof Uint8Array, toMeta: u8Array, priority: 50 },
+            { type: Base.JSONT_U16_ARRAY, check: (d) => d instanceof Uint16Array, toMeta: u16Array, priority: 50 },
+            { type: Base.JSONT_U32_ARRAY, check: (d) => d instanceof Uint32Array, toMeta: u32Array, priority: 50 },
+            { type: Base.JSONT_U64_ARRAY, check: (d) => d instanceof BigUint64Array, toMeta: u64Array, priority: 50 },
+            {
+                type: Base.JSONT_OBJECT,
+                check: (d): d is object => isPlainObject(d),
+                toMeta: (d) => object(...Object.entries(d).map((c) => field(c[0], meta.toMetadata(c[1])))),
+                priority: 100
+            }
+        ]
+        types.forEach(t => meta.addType(t))
         return meta
     }
 
