dc1fcc09aa701b073d28f3c042d5c5327896cc7f
User <ulyanovskiy.2001@mail.ru>
2026-06-30 16:46:17 +0300

    feat: update meta builder types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,26 +3,24 @@ export type ReadResult<T> = {
     readonly nextIndex: number
 }
 
-export abstract class BaseWrapper<T> {
+export class Nullable<T> {
     constructor(private readonly _value: T) { }
 
     public get value() {
         return this._value
     }
-}
 
-export class Nullable<T> extends BaseWrapper<T> {
     static new<T>(value: T) {
         return new Nullable<T>(value)
     }
 }
 
-export class Int8 extends BaseWrapper<number> { }
-export class Int16 extends BaseWrapper<number> { }
-export class Int32 extends BaseWrapper<number> { }
-export class Int64 extends BaseWrapper<number> { }
-export class Uint8 extends BaseWrapper<number> { }
-export class Uint16 extends BaseWrapper<number> { }
-export class Uint32 extends BaseWrapper<number> { }
-export class Uint64 extends BaseWrapper<number> { }
-export class Float32 extends BaseWrapper<number> { }
+export class Int8 { }
+export class Int16 { }
+export class Int32 { }
+export class Int64 { }
+export class Uint8 { }
+export class Uint16 { }
+export class Uint32 { }
+export class Uint64 { }
+export class Float32 { }
