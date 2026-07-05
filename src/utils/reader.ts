289b1e471fa4bdbeaf851005cc37458482f813b6
User <ulyanovskiy.2001@mail.ru>
2026-07-05 13:52:46 +0300

    feat: create basic reader wrapper for data to support async stream deserialization

---
+++ src/utils/reader.ts
@@ -0,0 +1,20 @@
+export default class JsonReader {
+    constructor(buffer: Uint8Array) {
+    }
+
+    get buffer() {
+        return new Uint8Array([])
+    }
+
+    get position() {
+        return 1
+    }
+
+    get length() {
+        return 1
+    }
+
+    public write(buffer: Uint8Array): void {
+
+    }
+}
