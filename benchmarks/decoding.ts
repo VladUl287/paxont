54df56478b630ce03ca4d5d16790eb239e13f1c3
User <ulyanovskiy.2001@mail.ru>
2026-07-09 09:28:14 +0300

    tests: update decoding benchmark

--- benchmarks/decoding.ts
+++ benchmarks/decoding.ts
@@ -1,18 +1,16 @@
 import { add, complete, cycle, suite } from 'benny'
-import { utf8toString } from '@exodus/bytes/utf8.js'
 
-const bytes = new TextEncoder().encode('datedatedatedatedatedatedate')
+const textDecoder = new TextDecoder('utf-8')
+const textEncoder = new TextEncoder()
+
+const bytes = textEncoder.encode('datedatedatedatedatedatedate')
 const bytesArr = [...bytes]
-const buffer = Buffer.from(bytes)
-const textDecoder = new TextDecoder()
 
 suite(
     'decoding',
 
+    add('fromCharCode', () => String.fromCharCode.apply(null, bytesArr)),
     add('default', () => textDecoder.decode(bytes)),
-    add('fromCharCode', () => String.fromCharCode.apply(String, bytesArr)),
-    add('buffer', () => buffer.toString()),
-    add('exodus-utf8', () => utf8toString(bytes)),
 
     cycle((result) => {
         const nanoseconds = (1 / result.ops) * 1e9
