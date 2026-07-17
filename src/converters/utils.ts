37f8a8e8a090a1d591323bf588be72e509a42558
User <ulyanovskiy.2001@mail.ru>
2026-07-17 15:20:54 +0300

    feat: simplify whitespace skip logic

--- src/converters/utils.ts
+++ src/converters/utils.ts
@@ -6,32 +6,13 @@ lookup[SPACE] = 1
 lookup[NEW_LINE] = 1
 lookup[CARRIAGE_RETURN] = 1
 
-export const isWhitespace = (b: number) => lookup[b]
-
 export function skipWhitespace(b: Uint8Array, i: number): number {
     if (b[i] > SPACE) return i
 
-    while (true) {
-        if (b[i] === SPACE) {
-            const word = b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24
+    while (i < b.length && lookup[b[i]]) i++
 
-            if (word === 0x20202020) {
-                i += 4
-                continue
-            }
+    if (i < b.length && b[i] < SPACE)
+        throw new Error(`Unexpected token ${String.fromCharCode(b[i])} at index ${i}`)
 
-            if ((word & 0xFFFF) === 0x2020) {
-                i += 2
-                continue
-            }
-
-            i++
-            continue
-        }
-        else if (lookup[b[i]]) i++
-        else break
-    }
-
-    if (b[i] < SPACE) throw new Error(`Unexpected token ${String.fromCharCode(b[i])} at index ${i}`)
     return i
 }
