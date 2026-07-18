97b4d2f668db3fef60bdb10e7a2c6c3305e960a2
User <ulyanovskiy.2001@mail.ru>
2026-07-18 15:48:38 +0300

    feat: create array recycler

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,10 +1,41 @@
-export function createFactory<T extends ArrayLike<any>>(ctor: new (length: number) => T) {
+type Indexable<V> = {
+    length: number
+    [index: number]: V
+}
+
+export type ArrayRecycler<T extends Indexable<V>, V> = {
+    acquire: (length: number, source?: T) => T,
+    dispose(): void
+}
+
+export function useArrayRecycler<T extends Indexable<V>, V>(ctor: new (length: number) => T): ArrayRecycler<T, V> {
     let array: T | null = null
-    return (length: number) => {
-        if (array && array.length >= length)
+
+    const acquire = (newLength: number, source?: T) => {
+        if (array !== null && array.length >= newLength) {
+            if (source) {
+                const length = Math.min(newLength, source.length)
+                for (let i = 0; i < length; i++)
+                    array[i] = source[i]
+            }
             return array
-        
-        array = new ctor(length)
+        }
+
+        array = new ctor(newLength)
+
+        if (source) {
+            const length = Math.min(newLength, source.length)
+            for (let i = 0; i < length; i++)
+                array[i] = source[i]
+        }
+
         return array
+    }
+
+    const dispose = () => (array = null)
+
+    return {
+        acquire,
+        dispose
     }
 }
