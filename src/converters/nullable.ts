be053c16aa2cac396de34f0ed98b62f76189315f
User <ulyanovskiy.2001@mail.ru>
2026-07-20 14:47:01 +0300

    feat: update nullable conveter to support partial converts

--- src/converters/nullable.ts
+++ src/converters/nullable.ts
@@ -1,24 +1,48 @@
 import { BaseMeta, ConvertState, JsonReader, NullableMeta } from "../metadata/types"
-import { ReadResult } from "../utils/types"
+import { isNeedsMoreData, ReadResult, ReadResultType } from "../utils/types"
 import { L, N, U } from "../utils/utf8constants"
 
 const NULL = N | U << 8 | L << 16 | L << 24
 
+const COMPLETE = ReadResultType.COMPLETE
+const NEEDS_MORE_DATA = ReadResultType.NEEDS_MORE_DATA
+
+type NullableState = ConvertState & {
+    valueState?: ConvertState
+}
+
 export function toNullable<T, M extends BaseMeta<T, M>>(
-    m: NullableMeta<T, M>,
+    metadata: NullableMeta<T, M>,
     reader: JsonReader,
-    i: number,
-    d: number,
-    s: ConvertState
+    index: number,
+    depth: number,
+    state: NullableState
 ): ReadResult<T | null> {
     const b = reader.bytes
     const len = b.length
 
+    let i = index
     if (i + 3 < len && (b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24) === NULL)
         return {
+            type: COMPLETE,
             value: null,
             nextIndex: i + 4
         }
 
-    return m.value.tryParseValue(reader, m.value, i, d)
+    if (i + 3 >= len && reader.writable)
+        return {
+            type: NEEDS_MORE_DATA,
+            nextIndex: i
+        }
+
+    const metaValue = metadata.value
+    const valueState = state.valueState ?? {}
+    const result = metaValue.tryParseValue(metaValue, reader, i, depth, valueState)
+
+    if (isNeedsMoreData(result)) {
+        state.isContinued = true
+        state.valueState = valueState
+    }
+
+    return result
 }
