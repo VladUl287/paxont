d47bedf3604fbb9b12b95eb314dc47583b07d677
User <ulyanovskiy.2001@mail.ru>
2026-07-20 13:37:25 +0300

    feat: reset state after successfull parsing

--- src/converters/bigint.ts
+++ src/converters/bigint.ts
@@ -15,44 +15,62 @@ export function tryParseBigInt(
     const len = b.length
 
     let start = i
-    i = state.lastIndex ?? i
+    if (state.isContinued)
+        i = state.lastIndex ?? i
 
-    while (i < len - 4) {
-        const a1 = b[i], a2 = b[i + 1], a3 = b[i + 2], a4 = b[i + 3]
+    try {
+        while (i < len - 4) {
+            const a1 = b[i], a2 = b[i + 1], a3 = b[i + 2], a4 = b[i + 3]
 
-        const word = (a1 << 0 | a2 << 8 | a3 << 16 | a4 << 24) - 0x30303030
-        const hasNonDigit = ((word + 0x76767676) | word) & 0x80808080
+            const word = (a1 << 0 | a2 << 8 | a3 << 16 | a4 << 24) - 0x30303030
+            const hasNonDigit = ((word + 0x76767676) | word) & 0x80808080
 
-        if (hasNonDigit !== 0) break
+            if (hasNonDigit !== 0) break
 
-        i += 4
-    }
+            i += 4
+        }
 
-    while (i < len && isDigitUnsafe(b[i])) i++
+        while (i < len && isDigitUnsafe(b[i])) i++
 
-    if (i === len && ctx.writable) {
-        state.lastIndex = i
+        if (i === len && ctx.writable) {
+            state.isContinued = true
+            state.lastIndex = i
 
+            return {
+                type: NEEDS_MORE_DATA,
+                nextIndex: start
+            }
+        }
+
+        const length = i - start
+        if (length <= 0) {
+            return {
+                type: ERROR,
+                error: new JSONParseError(
+                    `Expected at least one digit at index ${i}, but found '${String.fromCharCode(b[i])}' while parsing bigint`, i)
+            }
+        }
+
+        const decoder = ctx.options.decoder
+        const view = new Uint8Array(b.buffer, start, i - start)
         return {
-            type: NEEDS_MORE_DATA,
-            nextIndex: start
+            type: COMPLETE,
+            value: BigInt(decoder.decode(view)),
+            nextIndex: i
         }
     }
-
-    const length = i - start
-    if (length <= 0) {
+    catch (error) {
         return {
-            type: ERROR,
+            type: ReadResultType.ERROR,
             error: new JSONParseError(
-                `Expected at least one digit at index ${i}, but found '${String.fromCharCode(b[i])}' while parsing bigint`, i)
+                `Unexpected error while parsing bigint at index ${i}: ${error instanceof Error ? error.message : String(error)}`,
+                i, { cause: error })
         }
     }
-
-    const decoder = ctx.options.decoder
-    const view = new Uint8Array(b.buffer, start, i - start)
-    return {
-        type: COMPLETE,
-        value: BigInt(decoder.decode(view)),
-        nextIndex: i
+    finally {
+        if (state.isContinued) {
+            state.isContinued = false
+            state.lastIndex = 0
+        }
     }
 }
