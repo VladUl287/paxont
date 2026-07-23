79331625594a7371288ea6b771f9c82e75c08bc5
User <ulyanovskiy.2001@mail.ru>
2026-07-23 10:07:47 +0300

    feat: create cache type

--- src/cache/cache.ts
+++ src/cache/cache.ts
@@ -1,11 +1,15 @@
-export function createCache<K, V>() {
+export type CacheFactory<K, V> = {
+    getOrAdd: <T extends V>(key: K, factory: (key: K) => T) => T
+}
+
+export function createCache<K, V>(): CacheFactory<K, V> {
     const cache = new Map<K, V>()
 
     return {
         getOrAdd: <T extends V>(key: K, factory: (key: K) => T): T => {
             if (cache.has(key))
                 return cache.get(key) as T
-        
+
             const value = factory(key)
             cache.set(key, value)
             return value
