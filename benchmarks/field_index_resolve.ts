239bf20f000a2af94a2e802a25e6ec3252be3218
User <ulyanovskiy.2001@mail.ru>
2026-07-23 12:36:52 +0300

    tests: update benchmark for field index resolver

--- benchmarks/field_index_resolve.ts
+++ benchmarks/field_index_resolve.ts
@@ -60,23 +60,15 @@ const keys = Object.keys(big_unique_object).map(c => {
     return encoder.encode(c)
 })
 
-const switchMathcerWithoutPack = generateTrieSwitch(keys, { pack: false })
-const switchMathcerPackWithPack = generateTrieSwitch(keys, { pack: true })
+const switchMathcerWithoutPack = generateTrieSwitch(keys)
 
 suite(
     'field_index_resolve',
 
-    add('switchMathcerWithoutPack', () => {
+    add('switchMathcer', () => {
         let result = 0
         for (let i = 0; i < keys.length; i++) {
             result += switchMathcerWithoutPack(keys[i], 0)
-        }
-        return result
-    }),
-    add('switchMathcerPackWithPack', () => {
-        let result = 0
-        for (let i = 0; i < keys.length; i++) {
-            result += switchMathcerPackWithPack(keys[i], 0)
         }
         return result
     }),
