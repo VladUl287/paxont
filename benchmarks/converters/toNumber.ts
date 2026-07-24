79bf263911fdc0a1472770628b9f30b7f88afadd
User <ulyanovskiy.2001@mail.ru>
2026-07-24 10:46:49 +0300

    tests: add int64 parser cases

--- benchmarks/converters/toNumber.ts
+++ benchmarks/converters/toNumber.ts
@@ -4,27 +4,34 @@ import { defaultOptions } from '../../src/options'
 import { toInt16, toInt32, toInt8, toUint16, toUint32, toUint8 } from '../../src/converters/number/int'
 import { Stack } from '../../src/utils/structs'
 import { toFloat } from '../../src/converters/number/float'
+import { toInt64, toUint64 } from '../../src/converters/number/bigint'
 
 const encoder = new TextEncoder()
-const decoder = new TextDecoder()
 
 const u8_str = "123"
 const u16_str = "12345"
 const u32_str = "123456789"
 const f64_str = "123456789.12e-3"
+const u64_str = "18446744073709551600"
+const i64_str = "9223372036854775800"
 const u8 = encoder.encode(u8_str)
 const u16 = encoder.encode(u16_str)
 const u32 = encoder.encode(u32_str)
 const f64 = encoder.encode(f64_str)
+const u64 = encoder.encode(u64_str)
+const i64 = encoder.encode(i64_str)
 
 const u8Context: ParseContext = { reader: { bytes: u8, writable: false }, options: defaultOptions, stack: new Stack() }
 const u16Context: ParseContext = { reader: { bytes: u16, writable: false }, options: defaultOptions, stack: new Stack() }
 const u32Context: ParseContext = { reader: { bytes: u32, writable: false }, options: defaultOptions, stack: new Stack() }
 const f64Context: ParseContext = { reader: { bytes: f64, writable: false }, options: defaultOptions, stack: new Stack() }
+const u64Context: ParseContext = { reader: { bytes: u64, writable: false }, options: defaultOptions, stack: new Stack() }
+const i64Context: ParseContext = { reader: { bytes: i64, writable: false }, options: defaultOptions, stack: new Stack() }
 const metadata: PrimitiveMeta<number> = {} as any
+const metadataBigInt: PrimitiveMeta<bigint> = {} as any
 
 suite(
-    'decoding',
+    'toNumbers',
 
     add('toUint8', () => toUint8(metadata, u8Context, 0, 0)),
     add('toUint16', () => toUint16(metadata, u16Context, 0, 0)),
@@ -35,9 +42,14 @@ suite(
     add('toUint8_parse', () => JSON.parse(u8_str)),
     add('toUint16_parse', () => JSON.parse(u16_str)),
     add('toUint32_parse', () => JSON.parse(u32_str)),
-    
+
     add('toFloat', () => toFloat(metadata, f64Context, 0, 0)),
     add('toFloat_parse', () => JSON.parse(f64_str)),
+
+    add('toUint64', () => toUint64(metadataBigInt, u64Context, 0, 0)),
+    add('toInt64', () => toInt64(metadataBigInt, i64Context, 0, 0)),
+    add('toUint64_parse', () => JSON.parse(u64_str)),
+    add('toInt64_parse', () => JSON.parse(i64_str)),
 
     cycle((result) => {
         const nanoseconds = (1 / result.ops) * 1e9
