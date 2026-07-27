9bbf60f12d4a0c9780f956bd63fd924125a5e5b0
User <ulyanovskiy.2001@mail.ru>
2026-07-27 11:19:43 +0300

    tests: tofloat add more cases

--- tests/converters/numbers/toFloat.test.ts
+++ tests/converters/numbers/toFloat.test.ts
@@ -381,5 +381,43 @@ describe('tryParseFloat', () => {
 
       expect(result).toStrictEqual({ type: ReadResultType.COMPLETE, value: 123, nextIndex: 3 })
     })
+
+    test('parses decimal', () => {
+      const chunks = [toBytes("12."), toBytes("12.3")].reverse()
+
+      let ch
+      let result
+      let index = 0
+
+      const stack = new Stack<ConvertState>()
+      while ((ch = chunks.pop()) !== undefined) {
+        const ctx = { reader: { bytes: ch, writable: chunks.length !== 0 }, options: defaultOptions, stack }
+        result = toFloat({} as any, ctx, index, 0)
+        if (isNeedsMoreData(result)) {
+          index = result.nextIndex
+        }
+      }
+
+      expect(result).toStrictEqual({ type: ReadResultType.COMPLETE, value: 12.3, nextIndex: 4 })
+    })
+
+    test('parses scientific', () => {
+      const chunks = [toBytes("12e"), toBytes("12e-3")].reverse()
+
+      let ch
+      let result
+      let index = 0
+
+      const stack = new Stack<ConvertState>()
+      while ((ch = chunks.pop()) !== undefined) {
+        const ctx = { reader: { bytes: ch, writable: chunks.length !== 0 }, options: defaultOptions, stack }
+        result = toFloat({} as any, ctx, index, 0)
+        if (isNeedsMoreData(result)) {
+          index = result.nextIndex
+        }
+      }
+
+      expect(result).toStrictEqual({ type: ReadResultType.COMPLETE, value: 12e-3, nextIndex: 5 })
+    })
   })
 })
