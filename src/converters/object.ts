ee8d10bbd31110969ca3c1f42ebd81a309deb0a8
User <ulyanovskiy.2001@mail.ru>
2026-07-29 11:11:34 +0300

    feat: make toobject converter more generic

--- src/converters/object.ts
+++ src/converters/object.ts
@@ -1,6 +1,6 @@
 import { skipWhitespace } from "./utils"
 import { isError, isNeedsMoreData, ReadResult, ReadResultType } from "../utils/types"
-import { ParseContext, ObjectFromMeta, ObjectMeta } from "../metadata/types"
+import { ParseContext, ObjectFromMeta, ObjectMeta, BaseMeta, ObjectFieldMeta } from "../metadata/types"
 import { COLON, COMMA, CURLY_CLOSE, CURLY_OPEN, DOUBLE_QUOTE } from "../utils/ascii_symbols"
 import { JSONParseError } from "../utils/error"
 
@@ -8,7 +8,7 @@ const COMPLETE = ReadResultType.COMPLETE
 const ERROR = ReadResultType.ERROR
 const NEEDS_MORE_DATA = ReadResultType.NEEDS_MORE_DATA
 
-export function toObject<T extends Record<string, any>>(
+export function toObject<T extends Record<string, BaseMeta<any, any>>>(
     m: ObjectMeta<T>,
     context: ParseContext,
     index: number,
