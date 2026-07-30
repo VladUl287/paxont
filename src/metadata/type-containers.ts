1d622383c292dc0967d99fb47a5943b05caa2d38
User <ulyanovskiy.2001@mail.ru>
2026-07-30 12:42:25 +0300

    feat: add containers brand field

--- src/metadata/type-containers.ts
+++ src/metadata/type-containers.ts
@@ -6,11 +6,27 @@ export class Nullable<T> {
     }
 }
 
-export class Int8 { }
-export class Int16 { }
-export class Int32 { }
-export class Int64 { }
-export class Uint8 { }
-export class Uint16 { }
-export class Uint32 { }
-export class Uint64 { }
+export class Int8 {
+    __brand: 'Int8' = 'Int8'
+}
+export class Int16 {
+    __brand: 'Int16' = 'Int16'
+}
+export class Int32 {
+    __brand: 'Int32' = 'Int32'
+}
+export class Int64 {
+    __brand: 'Int64' = 'Int64'
+}
+export class Uint8 {
+    __brand: 'Uint8' = 'Uint8'
+}
+export class Uint16 {
+    __brand: 'Uint16' = 'Uint16'
+}
+export class Uint32 {
+    __brand: 'Uint32' = 'Uint32'
+}
+export class Uint64 {
+    __brand: 'Uint64' = 'Uint64'
+}
