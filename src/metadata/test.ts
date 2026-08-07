829dec4720a5eb352e8860acf86093328690d8fa
User <ulyanovskiy.2001@mail.ru>
2026-08-07 20:06:50 +0300

    feat: make modifier work

--- src/metadata/test.ts
+++ src/metadata/test.ts
@@ -1,18 +1,22 @@
 import { number, string } from "./builder"
-import { BaseMeta } from "./types"
-
-type ObjectMeta<T> = { [K in keyof T]: T[K] } & { [x: string]: any }
+import { BaseMeta, Obj, ObjectMeta } from "./types"
 
 type Expand<T> = T extends object ? { [K in keyof T]: T[K] } : T
 
-type FieldModifier<K extends string, V extends BaseMeta<any, any>> = <T>(obj: ObjectMeta<T>) => ObjectMeta<Expand<T & { [P in K]: V }>>
+type FieldModifier<K extends string, V extends BaseMeta<any, any>> = <T extends {}>(obj: ObjectMeta<T>) =>
+  ObjectMeta<Expand<T & { [P in K]: V }>>
 
-type ApplyModifier<Mod, Obj> =
-  Mod extends <T>(obj: ObjectMeta<T>) => ObjectMeta<infer R>
-  ? Obj extends ObjectMeta<infer U> ? Expand<U & R> : never
+declare function _field<K extends string, V extends BaseMeta<any, any>>(
+  name: K,
+  value: V
+): FieldModifier<K, V>
+
+type ApplyModifier<Mod, O> =
+  Mod extends (obj: ObjectMeta<any>) => ObjectMeta<infer R>
+  ? Expand<O & R>
   : never
 
-type CombineModifiers<Mods extends any[], Acc> =
+type CombineModifiers<Mods extends any[], Acc extends Obj> =
   Mods extends [infer First, ...infer Rest]
   ? CombineModifiers<Rest, ApplyModifier<First, Acc>>
   : Acc
@@ -20,11 +24,6 @@ type CombineModifiers<Mods extends any[], Acc> =
 declare function b<Mods extends any[]>(
   ...mods: Mods
 ): ObjectMeta<CombineModifiers<Mods, {}>>
-
-declare function _field<K extends string, V extends BaseMeta<any, any>>(
-  name: K,
-  value: V
-): FieldModifier<K, V>
 
 const result = b(
   _field('id', number()),
