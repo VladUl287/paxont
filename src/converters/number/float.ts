05dff5d54faf877ee712558b1147f1e68e20446d
User <ulyanovskiy.2001@mail.ru>
2026-08-10 10:50:23 +0300

    feat: make return exaclty non number byte

--- src/converters/number/float.ts
+++ src/converters/number/float.ts
@@ -68,7 +68,7 @@ function getNumberEndIndex(b: Uint8Array, i: number): number {
 
         const chunkLength = Math.min(i + 4, len)
         while (i < chunkLength) {
-            if (!isNumberByte(b[i])) { return i - 1 }
+            if (!isNumberByte(b[i])) { return i }
             i++
         }
     }
