68c1b45a75a3632d3526d60524e455ba9a1448f7
User <ulyanovskiy.2001@mail.ru>
2026-08-11 14:29:37 +0300

    Revert "feat: make object field itself as meta"
    This reverts commit 9474c63b369d60a40f2f77a79d625debfd7910b6.

--- src/code_gen/object.ts
+++ src/code_gen/object.ts
@@ -1,4 +1,4 @@
-import { ObjectMeta } from "../metadata/types"
+import { ObjectField, ObjectMeta } from "../metadata/types"
 import { nameof } from "../utils/types"
 
 export function genObjectFactory<M extends ObjectMeta<any>>(fields: M['fields']): M['build'] {
@@ -9,9 +9,10 @@ export function genObjectFactory<M extends ObjectMeta<any>>(fields: M['fields'])
 }
 
 export function genObjectToJsonFactory<M extends ObjectMeta<any>>(fields: M['fields']): M['toJson'] {
+    const value = nameof<ObjectField<any, any>>('value')
     const toJson = nameof<ObjectMeta<any>>('toJson')
     const field = fields
-        .map((field, i) => `"${field.name.value}":f[${i}].${toJson}(f[${i}],v.${field},o)`)
+        .map((field, i) => `"${field.name.value}":f[${i}].${value}.${toJson}(f[${i}].${value},v.${field},o)`)
         .join(',')
     return new Function('m', 'v', 'o', `var f = m.fields;return \`{${field}}\``) as M['toJson']
 }
