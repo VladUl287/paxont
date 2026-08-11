57b8ef914c69aaeabf0a6f03ea8b805316098b77
User <ulyanovskiy.2001@mail.ru>
2026-08-11 14:31:29 +0300

    refactor: remove unused code

--- src/metadata/builder.ts
+++ src/metadata/builder.ts
@@ -309,5 +309,3 @@ export const field = <K extends string, M extends BaseMeta<any, M>>(
         value: value
     }
 }
-
-export const builder = <M extends ObjectMeta<any>>(build: M['build']) => (m: M): M => ({ ...m, build })
