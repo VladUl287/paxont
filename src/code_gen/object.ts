3115d944ec53d420265c64a2522686ba7088b7db
User <ulyanovskiy.2001@mail.ru>
2026-08-11 14:12:29 +0300

    fix: object to json factory

--- src/code_gen/object.ts
+++ src/code_gen/object.ts
@@ -1,4 +1,4 @@
-import { BaseMeta, ObjectField, ObjectMeta } from "../metadata/types"
+import { ObjectField, ObjectMeta } from "../metadata/types"
 import { nameof } from "../utils/types"
 
 export function genObjectFactory<M extends ObjectMeta<any>>(fields: M['fields']): M['build'] {
@@ -9,13 +9,10 @@ export function genObjectFactory<M extends ObjectMeta<any>>(fields: M['fields'])
 }
 
 export function genObjectToJsonFactory<M extends ObjectMeta<any>>(fields: M['fields']): M['toJson'] {
-    let body = 'var f = m.fields;'
-    body += 'return `{'
-    const valueField = nameof<ObjectField<any, any>>('value')
-    const toJsonField = nameof<BaseMeta<any, any>>('toJson')
-    body += fields
-        .map((key, i) => `"${key.name.value}":\${f[${i}].${valueField}.${toJsonField}(f[${i}].${valueField},v.${key},o)}`)
+    const value = nameof<ObjectField<any, any>>('value')
+    const toJson = nameof<ObjectMeta<any>>('toJson')
+    const field = fields
+        .map((field, i) => `"${field.name.value}":f[${i}].${value}.${toJson}(f[${i}].${value},v.${field},o)`)
         .join(',')
-    body += '}`'
-    return new Function('m', 'v', 'o', body) as M['toJson']
+    return new Function('m', 'v', 'o', `var f = m.fields;return \`{${field}}\``) as M['toJson']
 }
