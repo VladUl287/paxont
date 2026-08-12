71d066d376b0a1d4cd79d3f2614e6cea54e8fd43
User <ulyanovskiy.2001@mail.ru>
2026-08-12 15:07:50 +0300

    feat: make set parser support partial parsing

--- src/converters/set.ts
+++ src/converters/set.ts
@@ -1,4 +1,4 @@
-import { BaseMeta, SetMeta, JsonParsingContext } from "../metadata/types"
+import { BaseMeta, SetMeta, JsonParsingContext, MetaValue } from "../metadata/types"
 import { COMMA, SQUARE_CLOSE, SQUARE_OPEN } from "../utils/ascii_symbols"
 import { skipWhitespace } from "./utils"
 import { isError, isNeedsMoreData, ReadResult, ReadResultType } from "../utils/types"
@@ -8,54 +8,50 @@ const COMPLETE = ReadResultType.COMPLETE
 const ERROR = ReadResultType.ERROR
 const NEEDS_MORE_DATA = ReadResultType.NEEDS_MORE_DATA
 
-export function toSet<V, M extends BaseMeta<V>>(
+export function toSet<M extends BaseMeta<MetaValue<M>>>(
     metadata: SetMeta<M>,
     context: JsonParsingContext,
-    index: number,
+    i: number,
     depth: number
-): ReadResult<Set<V>> {
-    const reader = context.reader
-    const options = context.options
+): ReadResult<Set<MetaValue<M>>> {
+    const { reader, options, stack } = context
 
-    if (depth > options.maxDepth)
+    if (depth > options.maxDepth) {
         return {
             type: ERROR,
-            error: new JSONParseError(`Maximum depth of ${options.maxDepth} exceeded at index ${index}`)
+            error: new JSONParseError(`Maximum depth exceeded`, { depth, index: i, metadata })
         }
+    }
 
     const b = reader.bytes
     const len = b.length
 
-    let i = index
     if (i >= len) {
-        if (reader.writable)
+        if (reader.writable) {
             return {
                 type: NEEDS_MORE_DATA,
                 nextIndex: i
             }
+        }
 
         return {
             type: ERROR,
-            error: new JSONParseError(`Unexpected end of input at index ${i} while parsing array`)
+            error: new JSONParseError(`Unexpected end of input`, { depth, index: i, metadata })
         }
     }
 
-    const stack = context.stack
     const state = stack.pop()
 
-    let isContinued: boolean
-
-    if (state !== undefined) {
-        isContinued = state.isContinued
-    }
-    else {
-        isContinued = false
-    }
+    const isContinued = state?.isContinued ?? false
+    const set: Set<MetaValue<M>> = state?.set ?? new Set<MetaValue<M>>()
+    let keySet: Set<any> | undefined = state?.keySet
 
     if (!isContinued) {
-        if (b[i] !== SQUARE_OPEN) return {
-            type: ERROR,
-            error: new JSONParseError(`Expected '[' but found '${String.fromCharCode(b[i])}'`, { depth, index: i, metadata })
+        if (b[i] !== SQUARE_OPEN) {
+            return {
+                type: ERROR,
+                error: new JSONParseError(`Expected '[' but found '${String.fromCharCode(b[i])}'`, { depth, index: i, metadata })
+            }
         }
         i++
 
@@ -66,45 +62,63 @@ export function toSet<V, M extends BaseMeta<V>>(
             }
         }
 
-        if (b[i] === SQUARE_CLOSE) return {
-            type: COMPLETE,
-            value: new Set<V>(),
-            nextIndex: ++i
+        if (b[i] === SQUARE_CLOSE) {
+            return {
+                type: COMPLETE,
+                value: new Set<MetaValue<M>>(),
+                nextIndex: ++i
+            }
         }
     }
 
-    const set = state?.set ?? new Set<V>()
-    const valueMeta = metadata.value
-
-    const tryParseValue = valueMeta.toValue
+    const keySelector = metadata.key
+    const itemMeta = metadata.value
+    const toValue = itemMeta.toValue
 
     while (true) {
         i = skipWhitespace(b, i)
 
-        const result = tryParseValue(valueMeta, context, i, depth)
+        if (b[i] === COMMA) { i++ }
+        else if (b[i] === SQUARE_CLOSE) { break }
+
+        const result = toValue(itemMeta, context, i, depth)
 
         if (isError(result))
             return result
 
         if (isNeedsMoreData(result)) {
-            stack.push({
-                isContinued: true,
-                set
-            })
+            stack.push({ isContinued: true, set, keySet })
             return result
         }
 
-        set.add(result.value)
-        i = result.nextIndex
+        if (keySelector) {
+            keySet ??= new Set()
 
-        i = skipWhitespace(b, i)
+            const key = keySelector(result.value)
+            if (!keySet.has(key)) {
+                keySet.add(key)
+                set.add(result.value)
+            }
+        }
+        else {
+            set.add(result.value)
+        }
 
-        if (b[i] === COMMA) i++
-        else if (b[i] === SQUARE_CLOSE) break
+        i = skipWhitespace(b, result.nextIndex)
+
+        if (b[i] === COMMA) { i++ }
+        else if (b[i] === SQUARE_CLOSE) { break }
+        else if (i >= b.length && reader.writable) {
+            stack.push({ isContinued: true, set, keySet })
+            return {
+                type: NEEDS_MORE_DATA,
+                nextIndex: i
+            }
+        }
         else {
             return {
                 type: ERROR,
-                error: new JSONParseError(`Unexpected end of value at index ${i} while parsing Set. Expected ']' or ',' as end of value`)
+                error: new JSONParseError(`Expected ']' or ',' but found '${String.fromCharCode(b[i])}'`, { depth, index: i, metadata })
             }
         }
     }
