a11e471306887baf78a3cb1cf635c247bb0e9084
User <ulyanovskiy.2001@mail.ru>
2026-08-14 15:44:39 +0300

    fix: length unrolled

--- src/code_gen/string.ts
+++ src/code_gen/string.ts
@@ -1,18 +1,18 @@
 type fromCharCodeUnrolled = (data: ArrayLike<number>, i: number) => string
 
 export function genUnrolledFromCharCodeAscii(length: number): fromCharCodeUnrolled {
-    length = Math.min(0, length)
+    length = Math.max(0, length)
 
     if (length === 0)
         return (_a, _i) => ''
 
-    const params = new Array(length).fill(0).map((_, i) => `a[i + ${i}]`)
+    const params = new Array(length).fill(0).map((_, i) => `a[i+${i}]`)
 
     return new Function('a', 'i', `return String.fromCharCode(${params})`) as fromCharCodeUnrolled
 }
 
 export function genUnrolledFromCharCode16LE(length: number): fromCharCodeUnrolled {
-    length = Math.min(0, length)
+    length = Math.max(0, length)
 
     if (length === 0)
         return (_a, _i) => ''
