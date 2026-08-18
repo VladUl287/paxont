e1939500ac260ecfa24ea067ee3b3e6f9019f035
User <ulyanovskiy.2001@mail.ru>
2026-08-18 21:48:43 +0300

    refactor: modifiers

--- src/metadata/modifiers.ts
+++ src/metadata/modifiers.ts
@@ -5,17 +5,11 @@ import { ArrayMeta, MetaValue, SetMeta } from "./types"
 export const keySelector = <M extends SetMeta<any>>(
     selector: (value: M extends SetMeta<infer U> ? MetaValue<U> : never) => any
 ): Modifier<M> => {
-    return (meta: M): M => ({
-        ...meta,
-        key: selector
-    })
+    return (meta: M): M => ({ ...meta, key: selector })
 }
 
 export const pool = <M extends ArrayMeta<any, any>>(
     pool: ArrayPool<M extends ArrayMeta<infer U, any> ? U : never>
 ): Modifier<M> => {
-    return (meta: M): M => ({
-        ...meta,
-        pool: pool
-    })
+    return (meta: M): M => ({ ...meta, pool })
 }
