c6f422e892262a6e231c4a682a44771768f7eb0b
User <ulyanovskiy.2001@mail.ru>
2026-08-19 19:13:41 +0300

    refactor: remove unused type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,5 @@
 export type Expand<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
 
-export type IsAny<T> = 0 extends (1 & T) ? true : false
-
 export function nameof<T>(key: Extract<keyof T, string>): Extract<keyof T, string> {
     return key
 }
