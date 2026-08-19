86a4df676fa73073c130db42924b55da7309ab27
User <ulyanovskiy.2001@mail.ru>
2026-08-19 13:58:14 +0300

    feat: create modifiers factory

--- src/metadata/modifiers.ts
+++ src/metadata/modifiers.ts
@@ -1,36 +1,45 @@
 import { ArrayPool } from "../utils/array"
 import { Modifier } from "./builder"
-import { ArrayMeta, BaseMeta, MetaValue, ObjectMeta, SetMeta } from "./types"
+import { ArrayMeta, BaseMeta, MetaValue, ObjectMeta, SetMeta, TypeName } from "./types"
 
-export const keySelector = <M extends SetMeta<any>>(
-    selector: (value: M extends SetMeta<infer U> ? MetaValue<U> : never) => any
-): Modifier<M> => {
-    return (meta: M): M => ({ ...meta, key: selector })
-}
+export const modifiers = () => {
+    return {
+        setModifiers: () => {
+            const keySelector = <M extends SetMeta<any>>(selector: (value: MetaValue<M['value']>) => any): Modifier<M> =>
+                (meta: M): M => ({ ...meta, key: selector })
 
-export const pool = <M extends ArrayMeta<any, any>>(
-    pool: ArrayPool<M extends ArrayMeta<infer U, any> ? U : never>
-): Modifier<M> => {
-    return (meta: M): M => ({ ...meta, pool })
-}
+            return { keySelector }
+        },
+        arrayModifiers: () => {
+            const pool = <M extends ArrayMeta<any, any>>(pool: ArrayPool<MetaValue<M>>): Modifier<M> =>
+                (meta: M): M => ({ ...meta, pool })
 
-export const builder = <M extends ObjectMeta<{}>>(build: M['build']) => {
-    return (m: M): M => ({ ...m, build })
-}
+            return { pool }
+        },
+        objectModifiers: () => {
+            const builder = <M extends ObjectMeta<{}>>(build: M['build']) =>
+                (m: M): M => ({ ...m, build })
 
-export const field = <K extends string, M extends BaseMeta<any>>(
-    name: K,
-    value: M,
-    encoder = new TextEncoder()
-): Modifier<ObjectMeta<{ [P in K]: M }>> => {
-    return (m) => {
-        m.fields.push({
-            name: {
-                value: name,
-                bytes: encoder.encode(name)
-            },
-            value: value
-        })
-        return m
+            const field = <K extends string, M extends BaseMeta<any>>(
+                name: K,
+                value: M,
+                encoder = new TextEncoder()
+            ): Modifier<ObjectMeta<{ [P in K]: M }>> => {
+                return (m) => {
+                    m.fields.push({
+                        name: {
+                            value: name,
+                            bytes: encoder.encode(name)
+                        },
+                        value: value
+                    })
+                    return m
+                }
+            }
+
+            return { builder, field }
+        }
     }
 }
+
+export const { objectModifiers, setModifiers, arrayModifiers } = modifiers()
