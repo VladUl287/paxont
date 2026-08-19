4ba9a87007974cb33ba7d999395fa1b02ac5ccf8
User <ulyanovskiy.2001@mail.ru>
2026-08-19 09:13:09 +0300

    refactor: nullable parser

--- src/converters/toValue/nullable.ts
+++ src/converters/toValue/nullable.ts
@@ -10,26 +10,27 @@ const NEEDS_MORE_DATA = ReadResultType.NEEDS_MORE_DATA
 export function toNullable<M extends BaseMeta<any>>(
     { value }: NullableMeta<M>,
     context: JsonParsingContext,
-    index: number,
+    i: number,
     depth: number
 ): ReadResult<MetaValue<M> | null> {
     const reader = context.reader
     const b = reader.bytes
     const len = b.length
 
-    let i = index
-    if (i + 3 < len && (b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24) === NULL)
+    if (i + 3 < len && (b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24) === NULL) {
         return {
             type: COMPLETE,
             value: null,
             nextIndex: i + 4
         }
+    }
 
-    if (i + 3 >= len && reader.writable)
+    if (i + 3 >= len && reader.writable) {
         return {
             type: NEEDS_MORE_DATA,
             nextIndex: i
         }
+    }
 
     return value.toValue(value, context, i, depth)
 }
