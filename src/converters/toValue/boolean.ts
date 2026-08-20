183ce7214bc6e62ae1746b9f81ab77b4d41e358e
User <ulyanovskiy.2001@mail.ru>
2026-08-20 15:26:03 +0300

    feat: boolean converter use actual byte length

--- src/converters/toValue/boolean.ts
+++ src/converters/toValue/boolean.ts
@@ -9,14 +9,12 @@ const NEEDS_MORE_DATA = ReadResultType.NEEDS_MORE_DATA
 
 export function toBoolean(
     m: PrimitiveMeta<boolean>,
-    { reader: { bytes: b, writable } }: JsonParsingContext,
+    { reader: { bytes: b, bytesLength: bytesLen, writable } }: JsonParsingContext,
     i: number, d: number
 ): ReadResult<boolean> {
-    const len = b.length
-
     let ch = 0
     const TRUE = 0x65757274
-    if (i + 3 < len && (ch = (b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24)) === TRUE)
+    if (i + 3 < bytesLen && (ch = (b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24)) === TRUE)
         return {
             type: COMPLETE,
             value: true,
@@ -24,14 +22,14 @@ export function toBoolean(
         }
 
     const FALSE = 0x736c6166
-    if (i + 4 < len && ch === FALSE && b[i + 4] === E)
+    if (i + 4 < bytesLen && ch === FALSE && b[i + 4] === E)
         return {
             type: COMPLETE,
             value: false,
             nextIndex: i + 5
         }
 
-    if (writable && (i + 3 >= len || i + 4 >= len))
+    if (writable && (i + 3 >= bytesLen || i + 4 >= bytesLen))
         return {
             type: NEEDS_MORE_DATA,
             nextIndex: i
