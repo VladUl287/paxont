a8676b7e0c9bcb681d7c2de1816c44c6d1781944
User <ulyanovskiy.2001@mail.ru>
2026-08-20 20:08:15 +0300

    tests: nullable tests

--- tests/converters/toNullable.test.ts
+++ tests/converters/toNullable.test.ts
@@ -2,8 +2,9 @@ import { bool, nullable } from "../../src/metadata/builder"
 import { JsonParsingContext, NullableMeta, PrimitiveMeta } from "../../src/metadata/types"
 import { defaultOptions } from "../../src/options"
 import { JSONParseError } from "../../src/utils/error"
+import { JsonReader } from "../../src/utils/reader"
+import { ReadResultType } from "../../src/utils/result"
 import { Stack } from "../../src/utils/stack"
-import { isComplete, ReadResultType } from "../../src/utils/types"
 import { deserializePartially } from "./utils"
 
 describe('toNullable', () => {
@@ -15,7 +16,7 @@ describe('toNullable', () => {
         const bytes = toBytes(str)
 
         const context: JsonParsingContext = {
-            reader: { bytes: bytes, writable: false },
+            reader: new JsonReader(bytes, bytes.length, false),
             options: defaultOptions,
             stack: new Stack()
         }
@@ -44,20 +45,25 @@ describe('toNullable', () => {
         const bytes = toBytes(str)
 
         const context: JsonParsingContext = {
-            reader: { bytes: bytes, writable: false },
+            reader: new JsonReader(bytes, bytes.length, false),
             options: defaultOptions,
             stack: new Stack()
         }
 
         const result = meta.toValue(meta, context, 0, 0)
 
-        expect(result).toStrictEqual({ type: ReadResultType.ERROR, error: expect.any(JSONParseError) })
+        expect(result).toStrictEqual({
+            type: ReadResultType.ERROR,
+            error: expect.any(JSONParseError)
+        })
 
         for (let i = 0; i < bytes.length; i++) {
             const chunks = [bytes.slice(0, i), bytes.slice(i)].reverse()
             const result = deserializePartially(meta, chunks)
-
-            expect(result).toStrictEqual({ type: ReadResultType.ERROR, error: expect.any(JSONParseError) })
+            expect(result).toStrictEqual({
+                type: ReadResultType.ERROR,
+                error: expect.any(JSONParseError)
+            })
         }
     }
 
