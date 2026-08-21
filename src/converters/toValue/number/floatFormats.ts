0ca087e77c8def7921c762eafd8966a480e02ac9
User <ulyanovskiy.2001@mail.ru>
2026-08-21 19:38:57 +0300

    refactor: remove unused data from format

--- src/converters/toValue/number/floatFormats.ts
+++ src/converters/toValue/number/floatFormats.ts
@@ -1,14 +1,6 @@
 export type FloatFormat = {
-    readonly normalMantissaBits: number
     readonly denormalMantissaBits: number
-    readonly exponentBias: number
     readonly maxBinaryExponent: number
-    readonly minBinaryExponent: number
-    readonly exponentBits: number
-    readonly normalMantissaMask: bigint
-    readonly denormalMantissaMask: bigint
-    readonly zeroBits: number
-    readonly overflowDecimalExponent: number,
     readonly maxExponentFastPath: number,
     readonly minSafeExponent: number,
     readonly maxSafeExponent: number,
@@ -18,16 +10,8 @@ export type FloatFormat = {
 }
 
 export const float64: FloatFormat = Object.freeze({
-    normalMantissaBits: 53,
     denormalMantissaBits: 52,
-    exponentBias: 1023,
     maxBinaryExponent: 1023,
-    minBinaryExponent: -1022,
-    exponentBits: 11,
-    normalMantissaMask: (1n << 53n) - 1n,
-    denormalMantissaMask: (1n << 52n) - 1n,
-    zeroBits: 0,
-    overflowDecimalExponent: 324,
     maxExponentFastPath: 22,
     minSafeExponent: -342,
     maxSafeExponent: 308,
