ca3432d15fa513ed3e705c485da80a31f0edc32a
User <ulyanovskiy.2001@mail.ru>
2026-08-21 15:21:35 +0300

    fix: make set use actual byteslength

--- src/converters/toValue/set.ts
+++ src/converters/toValue/set.ts
@@ -12,55 +12,38 @@ export function toSet<M extends BaseMeta<MetaValue<M>>>(
     metadata: SetMeta<M>,
     context: JsonParsingContext,
     i: number,
-    depth: number
+    d: number
 ): ReadResult<Set<MetaValue<M>>> {
     const { reader, options, stack } = context
 
-    if (depth > options.maxDepth) {
+    if (d > options.maxDepth) {
         return {
             type: ERROR,
-            error: new JSONParseError(`Maximum depth exceeded`, { depth, index: i, metadata })
+            error: new JSONParseError(`Maximum depth exceeded`, { depth: d, index: i, metadata })
         }
     }
 
-    const b = reader.bytes
-    const len = b.length
-
-    if (i >= len) {
-        if (reader.writable) {
-            return {
-                type: NEEDS_MORE_DATA,
-                nextIndex: i
-            }
-        }
-
-        return {
-            type: ERROR,
-            error: new JSONParseError(`Unexpected end of input`, { depth, index: i, metadata })
-        }
-    }
+    const { bytes: b, bytesLength: len, writable } = reader
 
     const state = stack.pop()
 
     const isContinued = state?.isContinued ?? false
     const set: Set<MetaValue<M>> = state?.set ?? new Set<MetaValue<M>>()
-    let keySet: Set<any> | undefined = state?.keySet
 
     if (!isContinued) {
         if (b[i] !== SQUARE_OPEN) {
+            if (i >= len && writable) {
+                return {
+                    type: NEEDS_MORE_DATA,
+                    nextIndex: i - 1
+                }
+            }
             return {
                 type: ERROR,
-                error: new JSONParseError(`Expected '[' but found '${String.fromCharCode(b[i])}'`, { depth, index: i, metadata })
+                error: new JSONParseError(`Expected '[' but found '${String.fromCharCode(b[i])}'`, { depth: d, index: i, metadata })
             }
         }
         i++
-
-        if (i >= b.length && reader.writable) {
-            return {
-                type: NEEDS_MORE_DATA,
-                nextIndex: i - 1
-            }
-        }
 
         if (b[i] === SQUARE_CLOSE) {
             return {
@@ -70,24 +53,39 @@ export function toSet<M extends BaseMeta<MetaValue<M>>>(
             }
         }
     }
+    else {
+        if (b[i] === COMMA) { i++ }
+        else if (b[i] === SQUARE_CLOSE) {
+            if (state?.hasComma) {
+                return {
+                    type: ERROR,
+                    error: new JSONParseError(`Trailing comma`)
+                }
+            }
+            return {
+                type: COMPLETE,
+                value: set,
+                nextIndex: ++i
+            }
+        }
+    }
 
     const keySelector = metadata.key
     const itemMeta = metadata.value
     const toValue = itemMeta.toValue
 
+    let hasComma = false
+    let keySet: Set<any> | undefined = state?.keySet
     while (true) {
         i = skipWhitespace(b, i)
 
-        if (b[i] === COMMA) { i++ }
-        else if (b[i] === SQUARE_CLOSE) { break }
-
-        const result = toValue(itemMeta, context, i, depth)
+        const result = toValue(itemMeta, context, i, d)
 
         if (isError(result))
             return result
 
         if (isNeedsMoreData(result)) {
-            stack.push({ isContinued: true, set, keySet })
+            stack.push({ isContinued: true, set, keySet, hasComma })
             return result
         }
 
@@ -106,20 +104,23 @@ export function toSet<M extends BaseMeta<MetaValue<M>>>(
 
         i = skipWhitespace(b, result.nextIndex)
 
-        if (b[i] === COMMA) { i++ }
+        if (b[i] === COMMA) {
+            i++
+            hasComma = true
+            continue
+        }
         else if (b[i] === SQUARE_CLOSE) { break }
-        else if (i >= b.length && reader.writable) {
+
+        if (i >= len && writable) {
             stack.push({ isContinued: true, set, keySet })
             return {
                 type: NEEDS_MORE_DATA,
                 nextIndex: i
             }
         }
-        else {
-            return {
-                type: ERROR,
-                error: new JSONParseError(`Expected ']' or ',' but found '${String.fromCharCode(b[i])}'`, { depth, index: i, metadata })
-            }
+        return {
+            type: ERROR,
+            error: new JSONParseError(`Expected ']' or ',' but found '${String.fromCharCode(b[i])}'`, { depth: d, index: i, metadata })
         }
     }
 
