7c2a80fe0a4afbdf810f95247fbf5c0a283a0a45
User <ulyanovskiy.2001@mail.ru>
2026-08-23 12:10:05 +0300

    refactor: boolean parser

--- src/converters/toValue/boolean.ts
+++ src/converters/toValue/boolean.ts
@@ -7,36 +7,37 @@ const COMPLETE = ReadResultType.COMPLETE
 const ERROR = ReadResultType.ERROR
 const NEEDS_MORE_DATA = ReadResultType.NEEDS_MORE_DATA
 
-export function toBoolean(
-    m: PrimitiveMeta<boolean>,
-    { reader: { bytes: b, bytesLength: bytesLen, writable } }: JsonParsingContext,
-    i: number, d: number
-): ReadResult<boolean> {
+export function toBoolean(meta: PrimitiveMeta<boolean>, context: JsonParsingContext): ReadResult<boolean> {
+    const { reader: { bytes: b, bytesLength: bytesLen, writable, position } } = context
+    let i = position
+
     let ch = 0
     const TRUE = 0x65757274
-    if (i + 3 < bytesLen && (ch = (b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24)) === TRUE)
+    if (i + 3 < bytesLen && (ch = (b[i] | b[i + 1] << 8 | b[i + 2] << 16 | b[i + 3] << 24)) === TRUE) {
         return {
             type: COMPLETE,
             value: true,
             nextIndex: i + 4
         }
+    }
 
     const FALSE = 0x736c6166
-    if (i + 4 < bytesLen && ch === FALSE && b[i + 4] === E)
+    if (i + 4 < bytesLen && ch === FALSE && b[i + 4] === E) {
         return {
             type: COMPLETE,
             value: false,
             nextIndex: i + 5
         }
+    }
 
-    if (writable && (i + 3 >= bytesLen || i + 4 >= bytesLen))
+    if (writable && (i + 3 >= bytesLen || i + 4 >= bytesLen)) {
         return {
             type: NEEDS_MORE_DATA,
             nextIndex: i
         }
-
+    }
     return {
         type: ERROR,
-        error: new JSONParseError(`Expected 'true' or 'false' but found '${String.fromCharCode(b[i])}'`, { metadata: m, index: i, depth: d })
+        error: new JSONParseError(`Expected 'true' or 'false' but found '${String.fromCharCode(b[i])}'`, { metadata: meta, index: i })
     }
 }
