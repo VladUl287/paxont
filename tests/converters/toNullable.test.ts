fd08077fda45784abd0d20df115a0022b1c3a7bd
User <ulyanovskiy.2001@mail.ru>
2026-08-25 14:12:24 +0300

    tests: update nullable connverter tests

--- tests/converters/toNullable.test.ts
+++ tests/converters/toNullable.test.ts
@@ -1,94 +1,31 @@
 import { bool, nullable } from "../../src/metadata/builder"
-import { JsonParsingContext, NullableMeta, PrimitiveMeta } from "../../src/metadata/types"
-import { defaultOptions } from "../../src/options"
-import { JSONParseError } from "../../src/utils/error"
-import { JsonReader } from "../../src/utils/reader"
-import { ReadResultType } from "../../src/utils/result"
-import { Stack } from "../../src/utils/stack"
-import { deserializePartially } from "./utils"
+import { expectError, expectToParse } from "./utils"
 
 describe('toNullable', () => {
-    const toBytes = (str: string) => new TextEncoder().encode(str)
-
     const meta = nullable(bool())
-
-    function expectNullable(meta: NullableMeta<PrimitiveMeta<boolean>>, str: string) {
-        const bytes = toBytes(str)
-
-        const context: JsonParsingContext = {
-            reader: new JsonReader(bytes, bytes.length, false),
-            options: defaultOptions,
-            stack: new Stack()
-        }
-
-        const result = meta.toValue(meta, context, 0, 0)
-
-        expect(result).toStrictEqual({
-            type: ReadResultType.COMPLETE,
-            value: JSON.parse(str),
-            nextIndex: bytes.length
-        })
-
-        for (let i = 0; i < bytes.length; i++) {
-            const chunks = [bytes.slice(0, i), bytes.slice(i)].reverse()
-            const result = deserializePartially(meta, chunks)
-
-            expect(result).toStrictEqual({
-                type: ReadResultType.COMPLETE,
-                value: JSON.parse(str),
-                nextIndex: chunks[0].length
-            })
-        }
-    }
-
-    function expectError(meta: NullableMeta<PrimitiveMeta<boolean>>, str: string) {
-        const bytes = toBytes(str)
-
-        const context: JsonParsingContext = {
-            reader: new JsonReader(bytes, bytes.length, false),
-            options: defaultOptions,
-            stack: new Stack()
-        }
-
-        const result = meta.toValue(meta, context, 0, 0)
-
-        expect(result).toStrictEqual({
-            type: ReadResultType.ERROR,
-            error: expect.any(JSONParseError)
-        })
-
-        for (let i = 0; i < bytes.length; i++) {
-            const chunks = [bytes.slice(0, i), bytes.slice(i)].reverse()
-            const result = deserializePartially(meta, chunks)
-            expect(result).toStrictEqual({
-                type: ReadResultType.ERROR,
-                error: expect.any(JSONParseError)
-            })
-        }
-    }
 
     describe('basic parsing', () => {
         test('should parse simple null', () => {
-            expectNullable(meta, 'null')
+            expectToParse({ meta, raw: 'null' })
         })
 
         test('should handle nullable value', () => {
-            expectNullable(meta, 'false')
-            expectNullable(meta, 'true')
+            expectToParse({ meta, raw: 'false' })
+            expectToParse({ meta, raw: 'true' })
         })
     })
 
     describe('error handling', () => {
         test('should throw error for invalid JSON', () => {
-            expectError(meta, 'NULL')
+            expectError({ meta, raw: 'NULL' })
         })
 
         test('should throw error for not enough data', () => {
-            expectError(meta, 'NU')
+            expectError({ meta, raw: 'NU' })
         })
 
         test('should throw error for invalid value JSON', () => {
-            expectError(meta, 'FALSE')
+            expectError({ meta, raw: 'FALSE' })
         })
     })
 })
