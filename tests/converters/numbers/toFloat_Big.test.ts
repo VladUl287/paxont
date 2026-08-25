10c07eb8f347ecb583e624eaa62319f0e705f8b7
User <ulyanovskiy.2001@mail.ru>
2026-08-25 19:34:58 +0300

    tests: update tofloat big test

--- tests/converters/numbers/toFloat_Big.test.ts
+++ tests/converters/numbers/toFloat_Big.test.ts
@@ -6,63 +6,39 @@ import { JsonReader } from '../../../src/utils/reader'
 import { float64 } from '../../../src/converters/toValue/number/floatFormats'
 import { tryParseFloat } from '../../../src/converters/toValue/number/float'
 import { ReadResultType } from '../../../src/utils/result'
+import { toBytes } from '../utils'
 
 describe('parseNumberF64-files', () => {
-    const encoder = new TextEncoder()
     const toContext = (str: string): JsonParsingContext => {
-        return { options: defaultOptions, reader: new JsonReader(encoder.encode(str), encoder.encode(str).length, false), stack: new Stack() }
+        const bytes = toBytes(str)
+        const reader = new JsonReader(bytes, bytes.length, false)
+        return new JsonParsingContext(reader, defaultOptions, new Stack())
     }
 
     const files = fs.readdirSync('./tests/data')
         .filter(file => file.endsWith('.txt'))
 
-    test.concurrent.each(files)('%s', (file) => {
-        const fileContent = fs.readFileSync('./tests/data/' + file)
-        const text = new TextDecoder().decode(fileContent)
+        files.forEach(file => {
+        test(file, () => {
+            const fileContent = fs.readFileSync('./tests/data/' + file)
+            const text = new TextDecoder().decode(fileContent)
 
-        const numbers = text.split('\n')
-            .filter(c => c.length > 0)
-            .map(l => {
+            const numbers = text.split('\n').map(l => {
                 const line = l.split(' ')
                 return line[line.length - 1]
             })
 
-        numbers.forEach(num => {
-            const ctx = toContext(num)
-            const parsed = tryParseFloat(ctx, 0, float64) as any
+            numbers.forEach(num => {
+                const reader = toContext(num)
+                const parsed = tryParseFloat(reader, float64) as any
 
-            parsed.value = `${num} -> ${parsed.value}`
-            expect(parsed).toStrictEqual({
-                type: ReadResultType.COMPLETE,
-                value: `${num} -> ${Number(num)}`,
-                nextIndex: ctx.reader.bytes.length
+                parsed.value = `${num} -> ${parsed.value}`
+                expect(parsed).toStrictEqual({
+                    type: ReadResultType.COMPLETE,
+                    value: `${num} -> ${Number(num)}`,
+                    nextIndex: reader.reader.bytes.length
+                })
             })
         })
     })
-
-    // const files = ['ulfjack-ryu.txt']
-
-    // files.forEach(file => {
-    //     test(file, () => {
-    //         const fileContent = fs.readFileSync('./tests/data/' + file)
-    //         const text = new TextDecoder().decode(fileContent)
-
-    //         const numbers = text.split('\n').map(l => {
-    //             const line = l.split(' ')
-    //             return line[line.length - 1]
-    //         })
-
-    //         numbers.forEach(num => {
-    //             const reader = toContext(num)
-    //             const parsed = tryParseFloat(reader, 0, float64) as any
-
-    //             parsed.value = `${num} -> ${parsed.value}`
-    //             expect(parsed).toStrictEqual({
-    //                 type: ReadResultType.COMPLETE,
-    //                 value: `${num} -> ${Number(num)}`,
-    //                 nextIndex: reader.reader.bytes.length
-    //             })
-    //         })
-    //     })
-    // })
 })
