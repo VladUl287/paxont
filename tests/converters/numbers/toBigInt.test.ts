7f1a393166f99a372321619bfbf97b9efc7467f9
User <ulyanovskiy.2001@mail.ru>
2026-08-25 19:20:35 +0300

    tests: use updated utils for bigint

--- tests/converters/numbers/toBigInt.test.ts
+++ tests/converters/numbers/toBigInt.test.ts
@@ -1,60 +1,7 @@
-import { array, bigInt } from "../../../src/metadata/builder"
-import { BaseMeta, JsonParsingContext } from "../../../src/metadata/types"
-import { defaultOptions } from "../../../src/options"
-import { JsonReader } from "../../../src/utils/reader"
-import { ReadResultType } from "../../../src/utils/result"
-import { Stack } from "../../../src/utils/stack"
-import { deserializePartially, expectError, toBytes } from "../utils"
+import { bigInt } from "../../../src/metadata/builder"
+import { expectError, expectToParse } from "../utils"
 
 describe('toBigInt', () => {
-  const expectToParse = <M extends BaseMeta<any>>(
-    options: {
-      meta: M,
-      raw?: string,
-      bytes?: Uint8Array,
-      start?: number,
-      end?: number,
-      depth?: number,
-      expected?: any
-    }) => {
-    let { meta, raw, bytes, start, end, depth, expected } = options
-
-    bytes ??= toBytes(raw ?? '')
-    start ??= 0
-    depth ??= 0
-    const ctx: JsonParsingContext = {
-      reader: new JsonReader(bytes, bytes.length, false, raw),
-      options: defaultOptions,
-      stack: new Stack(),
-    }
-
-    const decodedValue = new TextDecoder().decode(bytes.subarray(start, end))
-    const expectedResult = expected ?? JSON.parse(decodedValue)
-
-    const value = meta.toValue(meta, ctx, start, depth)
-    expect(value).toStrictEqual({
-      type: ReadResultType.COMPLETE,
-      value: expectedResult,
-      nextIndex: end ? end : bytes.length
-    })
-
-    for (let i = 0; i < bytes.length; i++) {
-      const chunks = [bytes.slice(0, i), bytes.slice(i, end)].reverse()
-      const result = deserializePartially(meta, chunks)
-
-      try {
-        expect(result).toStrictEqual({
-          type: ReadResultType.COMPLETE,
-          value: expectedResult,
-          nextIndex: chunks[0].length
-        })
-      } catch (error) {
-        console.log('error on: ', i)
-        throw error
-      }
-    }
-  }
-
   it('should parse usual bigint', () => {
     expectToParse({ meta: bigInt(), raw: '18446744073709551600', expected: 18446744073709551600n })
   })
@@ -74,6 +21,6 @@ describe('toBigInt', () => {
   })
 
   it('should throw error for non-numeric input', () => {
-    expectError(bigInt(), 'abs')
+    expectError({ meta: bigInt(), raw: 'abs' })
   })
 })
