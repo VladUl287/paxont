0700ed2f86085cd4fb64a2443f77ea70532ed9e2
User <ulyanovskiy.2001@mail.ru>
2026-09-03 15:16:08 +0300

    fix: array check condition

--- src/converters/toJson/array.ts
+++ src/converters/toJson/array.ts
@@ -1,13 +1,13 @@
 import { ArrayMeta, BaseMeta } from "../../metadata/types"
 
 export const arrayToJson: ArrayMeta<ArrayLike<any>, BaseMeta<any>>['toJson'] = (meta, value, options) => {
-    if (!Array.isArray(value) || !isTypedArray(value)) {
+    if (!Array.isArray(value) && !isTypedArray(value)) {
         throw new TypeError(`Expected Array, got ${typeof value}`)
     }
     const toJson = meta.value.toJson
     return `[${value.map(c => toJson(meta.value, c, options)).join(',')}]`
 }
 
-function isTypedArray(value: any): value is ArrayLike<any> {
+function isTypedArray(value: any): value is Array<any> {
     return ArrayBuffer.isView(value) && !(value instanceof DataView)
 }
