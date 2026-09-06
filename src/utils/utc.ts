04bd0ba87c1d7c75ae8e32990fff239c8954cb6a
User <ulyanovskiy.2001@mail.ru>
2026-09-06 13:15:59 +0300

    fix: date sub 1970 utc computation

--- src/utils/utc.ts
+++ src/utils/utc.ts
@@ -10,7 +10,7 @@ export function precomputeUTC(options: { minYear: number, maxYear: number }) {
 
     const daysYearsNew = new Int32Array(MAX_YEAR)
     for (let y = 1969; y >= MIN_YEAR; y--) {
-        daysYearsNew[y] = daysYearsNew[y + 1] - (isLeap(y + 1) ? 366 : 365)
+        daysYearsNew[y] = daysYearsNew[y + 1] - (isLeap(y) ? 366 : 365)
     }
 
     for (let y = 1971; y <= MAX_YEAR; y++) {
