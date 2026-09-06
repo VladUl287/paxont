a9f4df4d230abd1e25c5bef74cc92343f27c1119
User <ulyanovskiy.2001@mail.ru>
2026-09-06 10:35:17 +0300

    feat: update utc logic

--- src/utils/utc.ts
+++ src/utils/utc.ts
@@ -6,22 +6,23 @@ export function precomputeUTC(options: { minYear: number, maxYear: number }) {
         return (y % 4 === 0 && y % 100 !== 0) || (y % 400 === 0)
     }
 
-    const MAX_YEAR = options.maxYear
-    const MIN_YEAR = options.minYear
+    const { minYear: MIN_YEAR, maxYear: MAX_YEAR } = options
 
     const daysYearsNew = new Int32Array(MAX_YEAR)
-    for (let y = 1969; y >= MIN_YEAR; y--)
+    for (let y = 1969; y > MIN_YEAR; y--) {
         daysYearsNew[y] = daysYearsNew[y + 1] - (isLeap(y) ? 366 : 365)
+    }
 
-    for (let y = 1970; y < MAX_YEAR; y++)
-        daysYearsNew[y] = (isLeap(y) ? 366 : 365) + Math.max(daysYearsNew[y - 1], 0)
+    for (let y = 1971; y < MAX_YEAR; y++) {
+        daysYearsNew[y] = daysYearsNew[y - 1] + (isLeap(y) ? 366 : 365)
+    }
 
     function utc(year: number, month: number, day = 1, hours = 0, minutes = 0, seconds = 0, ms = 0) {
         if (year < MIN_YEAR || year > MAX_YEAR)
             return Date.UTC(year, month, day, hours, minutes, seconds, ms)
 
         const monthDays = isLeap(year) ? monthsLeap[month] : months[month]
-        const days = daysYearsNew[year - 1] + monthDays + (day - 1)
+        const days = daysYearsNew[year] + monthDays + (day - 1) - (year > 1970 ? 1 : 0)
         return days * 86400000 + hours * 3600000 + minutes * 60000 + seconds * 1000 + ms
     }
 
