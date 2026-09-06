f668e5e7267bbde3fd8999f697e15156314a6acf
User <ulyanovskiy.2001@mail.ru>
2026-09-06 10:51:59 +0300

    fix: count and save years before specific year and not included

--- src/utils/utc.ts
+++ src/utils/utc.ts
@@ -10,11 +10,11 @@ export function precomputeUTC(options: { minYear: number, maxYear: number }) {
 
     const daysYearsNew = new Int32Array(MAX_YEAR)
     for (let y = 1969; y > MIN_YEAR; y--) {
-        daysYearsNew[y] = daysYearsNew[y + 1] - (isLeap(y) ? 366 : 365)
+        daysYearsNew[y] = daysYearsNew[y + 1] - (isLeap(y + 1) ? 366 : 365)
     }
 
     for (let y = 1971; y < MAX_YEAR; y++) {
-        daysYearsNew[y] = daysYearsNew[y - 1] + (isLeap(y) ? 366 : 365)
+        daysYearsNew[y] = daysYearsNew[y - 1] + (isLeap(y - 1) ? 366 : 365)
     }
 
     function utc(year: number, month: number, day = 1, hours = 0, minutes = 0, seconds = 0, ms = 0) {
@@ -22,7 +22,7 @@ export function precomputeUTC(options: { minYear: number, maxYear: number }) {
             return Date.UTC(year, month, day, hours, minutes, seconds, ms)
 
         const monthDays = isLeap(year) ? monthsLeap[month] : months[month]
-        const days = daysYearsNew[year] + monthDays + (day - 1) - (year > 1970 ? 1 : 0)
+        const days = daysYearsNew[year] + monthDays + (day - 1)
         return days * 86400000 + hours * 3600000 + minutes * 60000 + seconds * 1000 + ms
     }
 
